Fixed some more bugs in the qick_asm Added single_shot threshold averaging removed the avg_amp from return values cleaned up both the averaging programs
Updated qubit demos to use threshold averaging, wrote new active reset demo

qick_lib/qick/averager_program.py
@@ -56,7 +56,7 @@ class AveragerProgram(QickProgram):
        
         p.end()        
         
-    def acquire(self, soc, load_pulses=True, progress=True, debug=False):
+    def acquire_round(self, soc, threshold=None, angle=[0,0], load_pulses=True, progress=True, debug=False):
         """
         This method optionally loads pulses on to the SoC, configures the ADC readouts, loads the machine code representation of the AveragerProgram onto the SoC, starts the program and streams the data into the Python, returning it as a set of numpy arrays.
 
@@ -136,16 +136,37 @@ class AveragerProgram(QickProgram):
         self.di_buf=di_buf
         self.dq_buf=dq_buf
         
-        #Average all of the data into a single point
-        avg_di0=np.sum(di_buf[0])/(reps)/self.cfg['adc_lengths'][0]
-        avg_dq0=np.sum(dq_buf[0])/(reps)/self.cfg['adc_lengths'][0]
-        avg_amp0=np.sqrt(avg_di0**2+avg_dq0**2)
+        #Average all of the data into a single point        
+        if threshold is None:
+            avg_di= np.array([np.sum(di_buf[0])/(reps)/self.cfg['adc_lengths'][0],np.sum(di_buf[1])/(reps)/self.cfg['adc_lengths'][1]])
+            avg_dq= np.array([np.sum(dq_buf[0])/(reps)/self.cfg['adc_lengths'][0],np.sum(dq_buf[1])/(reps)/self.cfg['adc_lengths'][1]])
+        else:
+            self.shots=self.get_single_shots(di_buf,dq_buf, threshold, angle)
+            avg_di=np.array([np.sum(self.shots[0])/(reps),np.sum(self.shots[1])/(reps)])
+            avg_dq=np.zeros(avg_di.shape)
         
-        avg_di1=np.sum(di_buf[1])/(reps)/self.cfg['adc_lengths'][1]
-        avg_dq1=np.sum(dq_buf[1])/(reps)/self.cfg['adc_lengths'][1]
-        avg_amp1=np.sqrt(avg_di1**2+avg_dq1**2)        
+        return avg_di, avg_dq
+    
+    def acquire(self, soc, threshold=None, angle=[0,0], load_pulses=True, progress=True, debug=False):
+        if "rounds" not in self.cfg or self.cfg["rounds"]==1:
+            return self.acquire_round(soc,threshold=threshold, angle=angle, load_pulses=load_pulses,progress=progress, debug=debug)
         
-        return avg_di0, avg_dq0, avg_amp0,avg_di1, avg_dq1, avg_amp1
+        avg_di=None
+        for ii in tqdm(range (self.cfg["rounds"]), disable=not progress):           
+            avg_di0, avg_dq0, avg_amp0=self.acquire_round(soc,threshold=threshold, angle=angle, load_pulses=load_pulses,progress=False, debug=debug)
+            
+            if avg_di is None:
+                avg_di, avg_dq = avg_di0, avg_dq0
+            else:
+                avg_di+= avg_di0
+                avg_dq+= avg_dq0
+                
+        return expt_pts, avg_di/self.cfg["rounds"], avg_dq/self.cfg["rounds"]
+    
+    def get_single_shots(self, di, dq, threshold, angle=[0,0]):
+        if type(threshold) is int:
+            threshold=[threshold,threshold]            
+        return np.array([np.heaviside((di[ch]*np.cos(angle[ch]) - dq[ch]*np.sin(angle[ch]))/self.cfg['adc_lengths'][ch]-threshold[ch],0) for ch in range(2)])        
 
     def acquire_decimated(self, soc, load_pulses=True, progress=True, debug=False):
         """
@@ -218,7 +239,7 @@ class AveragerProgram(QickProgram):
             di_avg1+=di1
             dq_avg1+=dq1
             
-        return di_avg0/soft_avgs,dq_avg0/soft_avgs, di_avg1/soft_avgs, dq_avg1/soft_avgs
+        return np.array([di_avg0,di_avg0])/soft_avgs,np.array([dq_avg1,dq_avg1])/soft_avgs
     
 class RAveragerProgram(QickProgram):
     """
@@ -298,7 +319,7 @@ class RAveragerProgram(QickProgram):
         """
         return self.cfg["start"]+np.arange(self.cfg['expts'])*self.cfg["step"]
         
-    def acquire(self, soc, load_pulses=True, ReadoutPerExpt=1, SaveExperiments=[0], progress=True, debug=False):
+    def acquire_round(self, soc, threshold=None, angle=[0,0], load_pulses=True, readouts_per_experiment=1, save_experiments=[0], progress=True, debug=False):
         """
         This method optionally loads pulses on to the SoC, configures the ADC readouts, loads the machine code representation of the RAveragerProgram onto the SoC, starts the program and streams the data into the Python, returning it as a set of numpy arrays.
 
@@ -311,22 +332,19 @@ class RAveragerProgram(QickProgram):
         :type soc: Qick object
         :param load_pulses: If true, loads pulses into the tProc
         :type load_pulses: bool
-        :param ReadoutPerExpt: How many measurements per experiment (>1 in experiments with conditional reset or just multiple measurements in same experiment.
-        :type ReadoutPerExpt: int
-        :param SaveExperiments: List of indices of experiments to keep (lets one skip conditional reset experiments).
-        :type SaveExperiments: list
+        :param readouts_per_experiment: How many measurements per experiment (>1 in experiments with conditional reset or just multiple measurements in same experiment.
+        :type readouts_per_experiment: int
+        :param save_experiments: List of indices of experiments to keep (lets one skip conditional reset experiments).
+        :type save_experiments: list
         :param progress: If true, displays progress bar
         :type progress: bool
         :param debug: If true, displays assembly code for tProc program
         :type debug: bool
         :returns:
             - expt_pts (:py:class:`list`) - list of experiment points
-            - avg_di0 (:py:class:`list`) - list of averaged accumulated I data ADC 0
-            - avg_dq0 (:py:class:`list`) - list of averaged accumulated Q data ADC 0
-            - amp_pts0 (:py:class:`list`) - list of averaged accumulated amplitude data ADC 0
-            - avg_di1 (:py:class:`list`) - list of averaged accumulated I data ADC 1
-            - avg_dq1 (:py:class:`list`) - list of averaged accumulated Q data ADC 1
-            - amp_pts1 (:py:class:`list`) - list of averaged accumulated amplitude data ADC 1
+            - avg_di (:py:class:`list`) - list of averaged accumulated I data ADC 0
+            - avg_dq (:py:class:`list`) - list of averaged accumulated Q data ADC 0
+            - amp_pts (:py:class:`list`) - list of averaged accumulated amplitude data ADC 0
         """
 
         if load_pulses: 
@@ -349,7 +367,7 @@ class RAveragerProgram(QickProgram):
         
         count=0
         last_count=0
-        total_count=reps*expts*ReadoutPerExpt
+        total_count=reps*expts*readouts_per_experiment
 
         di_buf=np.zeros((2,total_count))
         dq_buf=np.zeros((2,total_count))
@@ -362,7 +380,7 @@ class RAveragerProgram(QickProgram):
         with tqdm(total=total_count, disable=not progress) as pbar:
             soc.tproc.start()
             while count<total_count-1:
-                count = soc.tproc.single_read(addr= 1)*ReadoutPerExpt
+                count = soc.tproc.single_read(addr= 1)*readouts_per_experiment
 
                 if count>=min(last_count+1000,total_count-1):
                     addr=last_count % soc.avg_bufs[1].AVG_MAX_LENGTH
@@ -382,22 +400,44 @@ class RAveragerProgram(QickProgram):
         self.di_buf=di_buf
         self.dq_buf=dq_buf
         
+        if threshold is not None:
+            self.shots=self.get_single_shots(di_buf,dq_buf, threshold, angle)
+                
         expt_pts=self.get_expt_pts()
         
-        if SaveExperiments==[]:
-            return expt_pts,di_buf,dq_buf
-        else:
-            avg_di=np.zeros((2,len(SaveExperiments),expts))
-            avg_dq=np.zeros((2,len(SaveExperiments),expts))
-            avg_amp=np.zeros((2,len(SaveExperiments),expts))
+
+        avg_di=np.zeros((2,len(save_experiments),expts))
+        avg_dq=np.zeros((2,len(save_experiments),expts))
+
+        for nn,ii in enumerate(save_experiments):
+            for ch in range (2):
+                if threshold is None:
+                    avg_di[ch][nn]=np.sum(di_buf[ch][ii::readouts_per_experiment].reshape((expts, reps)),1)/(reps)/self.cfg['adc_lengths'][ch]
+                    avg_dq[ch][nn]=np.sum(dq_buf[ch][ii::readouts_per_experiment].reshape((expts, reps)),1)/(reps)/self.cfg['adc_lengths'][ch]
+                else:
+                    avg_di[ch][nn]=np.sum(self.shots[ch][ii::readouts_per_experiment].reshape((expts, reps)),1)/(reps)
+                    avg_dq=np.zeros(avg_di.shape)
+
+        return expt_pts, avg_di, avg_dq
+
+    def get_single_shots(self, di, dq, threshold, angle=[0,0]):
+        if type(threshold) is int:
+            threshold=[threshold,threshold]            
+        return np.array([np.heaviside((di[ch]*np.cos(angle[ch]) - dq[ch]*np.sin(angle[ch]))/self.cfg['adc_lengths'][ch]-threshold[ch],0) for ch in range(2)])
+                    
+    def acquire(self, soc, threshold=None, angle=[0,0], load_pulses=True, readouts_per_experiment=1, save_experiments=[0], progress=True, debug=False):
+        if "rounds" not in self.cfg or self.cfg["rounds"]==1:
+            return self.acquire_round(soc, threshold=threshold, angle=angle, readouts_per_experiment=readouts_per_experiment, save_experiments=save_experiments, load_pulses=load_pulses, progress=progress, debug=debug)
         
-            for nn,ii in enumerate(SaveExperiments):
-                avg_di[0][nn]=np.sum(di_buf[0][ii::ReadoutPerExpt].reshape((expts, reps)),1)/(reps)/self.cfg['adc_lengths'][0]
-                avg_dq[0][nn]=np.sum(dq_buf[0][ii::ReadoutPerExpt].reshape((expts, reps)),1)/(reps)/self.cfg['adc_lengths'][0]
-                avg_amp[0][nn]=np.sqrt(avg_di[0][nn]**2+avg_dq[0][nn]**2)
+        avg_di=None
+        for ii in tqdm(range (self.cfg["rounds"]), disable=not progress):
+            expt_pts, avg_di0, avg_dq0=self.acquire_round(soc, threshold=threshold, angle=angle, readouts_per_experiment=readouts_per_experiment, save_experiments=save_experiments, load_pulses=load_pulses, progress=False, debug=debug)
             
-                avg_di[1][nn]=np.sum(di_buf[1][ii::ReadoutPerExpt].reshape((expts, reps)),1)/(reps)/self.cfg['adc_lengths'][0]
-                avg_dq[1][nn]=np.sum(dq_buf[1][ii::ReadoutPerExpt].reshape((expts, reps)),1)/(reps)/self.cfg['adc_lengths'][0]
-                avg_amp[1][nn]=np.sqrt(avg_di[1][nn]**2+avg_dq[1][nn]**2)
+            if avg_di is None:
+                avg_di, avg_dq= avg_di0, avg_dq0
+            else:
+                avg_di+= avg_di0
+                avg_dq+= avg_dq0
         
-            return expt_pts, avg_di, avg_dq, avg_amp
+                
+        return expt_pts, avg_di/self.cfg["rounds"], avg_dq/self.cfg["rounds"]