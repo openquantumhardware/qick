970c377903ca9ef0be15bf396cfcc8566ad6be8f
Sho Uemura <suemura@fnal.gov>
2022-02-16 13:42:39 -0600

    extract channel mapping from HWH parser - should be more flexible for mixing IP types

--- qick_lib/qick/helpers.py
+++ qick_lib/qick/helpers.py
@@ -39,3 +39,46 @@ def triang(length=100,maxv=30000):
     y = np.concatenate((y1,y2))
     y = y/np.max(y)*maxv
     return y
+
+def trace_net(parser, blockname, portname):
+    """
+    Find the block and port that connect to this block and port.
+    If you expect to only get one block+port as a result, you can assign the result to ((block, port),)
+
+    :param parser: HWH parser object (from Overlay.parser, or BusParser)
+    :param blockname: the IP block of interest
+    :type blockname: string
+    :param portname: the port we want to trace
+    :type portname: string
+
+    :return: a list of (block, port) pairs
+    :rtype: list
+    """
+
+    fullport = blockname+"/"+portname
+    # the net connected to this port
+    netname = parser.pins[fullport]
+    # get the list of other ports on this net, discard the port we started at and ILA ports
+    return [x.split('/') for x in parser.nets[netname] if x!=fullport and 'system_ila_' not in x]
+
+class BusParser:
+    def __init__(self, parser):
+        """
+        Matching all the buses in the modules from the HWH file.
+        This is essentially a copy of the HWH parser's match_nets() and match_pins(),
+        but working on buses instead of signals.
+
+        :param parser: HWH parser object (from Overlay.parser)
+        """
+        self.nets = {}
+        self.pins = {}
+        for module in parser.root.findall('./MODULES/MODULE'):
+            fullpath = module.get('FULLNAME').lstrip('/')
+            for bus in module.findall('./BUSINTERFACES/BUSINTERFACE'):
+                port = fullpath + '/' + bus.get('NAME')
+                busname = bus.get('BUSNAME')
+                self.pins[port] = busname
+                if busname in self.nets:
+                    self.nets[busname] |= set([port])
+                else:
+                    self.nets[busname] = set([port])
