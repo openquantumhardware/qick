d28c50fcbf8978ec5fe65f1a230ca660b1db4972
David Winter <dastw@gmx.net>
2022-07-18 13:45:25 +0200

    Fix setup.py to not import required modules

--- setup.py
+++ setup.py
@@ -1,9 +1,38 @@
 from distutils.core import setup
-from ipq_pynq_utils import utils
+
+import platform
+
+def _parse_version_string(s):
+    arr = [int(v) for v in s.split(".")]
+    return arr + [0]*(3-len(arr))
+
+def _compare_version_lt(a, b):
+    if a[0] < b[0]:
+        return True
+    elif a[0] > b[0]:
+        return False
+
+    if a[1] < b[1]:
+        return True
+    elif a[1] > b[1]:
+        return False
+
+    if a[2] < b[2]:
+        return True
+
+    return False
+
+_python_version = _parse_version_string(platform.python_version())
+
+def python_version_lt(a):
+    if isinstance(a, str):
+        return _compare_version_lt(_python_version, _parse_version_string(a))
+    else:
+        return _compare_version_lt(_python_version, a)
 
 dependencies = ["spidev>=3.5"]
 
-if utils.python_version_lt("3.10"):
+if python_version_lt("3.10"):
     dependencies.append("importlib_resources")
 
 setup(name='ipq_pynq_utils',
