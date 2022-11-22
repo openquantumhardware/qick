4e407cf750110bf1f5c526ed24874fe4ed4c6001
Sho Uemura <suemura@fnal.gov>
2022-11-21 19:32:39 -0600

    add credential caching and refreshing

--- aws/user_client.py
+++ aws/user_client.py
@@ -3,7 +3,9 @@
 import logging
 import argparse
 import json
+import base64
 import getpass
+import time
 import datetime
 import sys
 import os
@@ -12,80 +14,75 @@ from configparser import ConfigParser
 import requests
 from fire import Fire
 
-class UserClient():
+class CognitoAuth(requests.auth.AuthBase):
+    TOKEN_PATH = os.path.expanduser('~/.cache/qick.tokens')
     def __init__(self):
-        configpaths = [os.path.expanduser('~/.config/qick/config'),
-                '/etc/qick/config']
+        self.auth_url = None
+        self.client_id = None
+        self.username = None
+        self.pool_id = None
+        self.expire_time = None
+        self.tokens = None
 
-        self.config = ConfigParser()
-        self.config.read(configpaths)
+        if os.path.exists(self.TOKEN_PATH):
+            with open(TOKEN_PATH, 'rt') as f:
+                self.update_tokens(json.load(f), replace_tokens=True, write_tokens=False)
 
+    def __call__(self, r):
+        """This is called by the Session to set auth headers.
+        """
+        if self.tokens is None:
+            self.initial_auth()
 
-        client_id = self.config['service']['cognito_clientid']
-        username = self.config['user']['username']
-        auth_url = self.config['service']['cognito_url']
+        # if we have less than 60 seconds till expiry, refresh tokens
+        if self.expire_time - time.time() < 60:
+            self.refresh_auth()
 
-        self.api_url = self.config['service']['api_url']
+        r.headers['Authorization'] = self.auth_header
+        return r
 
-        #pool_id = self.config['service']['cognito_userpool'] # only needed for SRP
-        #tokens = self._do_auth_srp(auth_url, client_id, username, pool_id)
-        #tokens = self._do_auth_srp_warrant(auth_url, client_id, username, pool_id)
+    def update_tokens(self, tokens, replace_tokens=False, write_tokens=True):
+        if replace_tokens or self.tokens is None:
+            self.tokens = tokens
+        else:
+            self.tokens.update(tokens)
 
-        tokens = self._do_auth_password(auth_url, client_id, username)
-        if 'AuthenticationResult' not in tokens:
+        access_token = self.tokens['AccessToken']
+        # unpack the JWT to get the expiry timestamp
+        # JWT uses unpadded base64, need to add dummy padding:
+        # https://stackoverflow.com/questions/2941995/python-ignore-incorrect-padding-error-when-base64-decoding
+        payload = json.loads(base64.b64decode(access_token.split('.')[1] + '=='))
+        self.expire_time = payload['exp']
+        self.auth_header = ' '.join([self.tokens['TokenType'], access_token])
+
+        if write_tokens:
+            logging.info("writing updated tokens")
+            with open(self.TOKEN_PATH, 'wt') as f:
+                json.dump(tokens, f)
+
+    def initial_auth(self):
+        logging.info("initial auth")
+        auth_response = self._do_auth_srp()
+        #auth_response = self._do_auth_srp_warrant()
+        #auth_response = self._do_auth_password()
+        if 'AuthenticationResult' not in auth_response:
             raise RuntimeError("Login failed")
-        auth_result = tokens['AuthenticationResult']
+        self.update_tokens(auth_response['AuthenticationResult'], replace_tokens=True)
 
-        self.session = requests.Session()
-        self.session.headers['Authorization'] = ' '.join([auth_result['TokenType'], auth_result['AccessToken']])
+    def refresh_auth(self):
+        logging.info("refreshing tokens")
+        refresh_response = self._auth_refresh(self.tokens['RefreshToken'])
+        if 'AuthenticationResult' not in refresh_response:
+            raise RuntimeError("Login failed")
+        # update tokens
+        self.update_tokens(refresh_response['AuthenticationResult'])
 
-    def add_user(self, email, fullname):
-        data = {
-                "Email": email,
-                "FullName": fullname
-                }
-        rsp = self.session.post(self.api_url + 'users', json=data)
-        if rsp.status_code == 200:
-            print("User successfully added! They should check their e-mail for a temporary password.")
-        else:
-            logging.warning(f"AddUser API error: {rsp.status_code}, {rsp.content}")
-
-    def add_device(self, device_name, refresh_timeout=60):
-        data = {
-                "DeviceName": device_name,
-                "RefreshTimeout": refresh_timeout
-                }
-        rsp = self.session.post(self.api_url + 'devices', json=data)
-        if rsp.status_code == 201:
-            rsp = rsp.json()
-            print("Device successfully added!")
-            print()
-            print("Put the following in the config file:")
-            print("[device]")
-            print(f"name = {rsp['DeviceName']}")
-            print(f"id = {rsp['DeviceId']}")
-            print()
-            print("Put the following in the device credentials file:")
-            print("[credentials]")
-            print(f"id = {rsp['ClientId']}")
-            print(f"secret = {rsp['ClientSecret']}")
-        else:
-            logging.warning(f"AddDevice API error: {rsp.status_code}, {rsp.content}")
-
-    def get_devices(self):
-        rsp = self.session.get(self.api_url + 'devices')
-        if rsp.status_code == 200:
-            return rsp.json()
-        else:
-            logging.warning(f"GetDevices API error: {rsp.status_code}, {rsp.content}")
-            return None
-
-    def _do_auth_srp(self, auth_uri, client_id, username, pool_id):
+    def _do_auth_srp(self):
         """this uses pysrp (standard SRP implementation) with patches from warrant (Cognito-specific)
         https://stackoverflow.com/questions/41526205/implementing-user-srp-auth-with-python-boto3-for-aws-cognito
         """
         import srp
-        import six, hmac, hashlib, base64
+        import six, hmac, hashlib
         def long_to_bytes(n):
             l = list()
             x = 0
@@ -172,21 +169,24 @@ class UserClient():
 
         _, A = usr.start_authentication()
         data = {"AuthFlow": "USER_SRP_AUTH",
-                "ClientId": client_id,
-                "AuthParameters": {"USERNAME": username,
+                "ClientId": self.client_id,
+                "AuthParameters": {"USERNAME": self.username,
                     "SRP_A": A.hex()}
                 }
         headers = {"X-Amz-Target": "AWSCognitoIdentityProviderService.InitiateAuth",
                 "Content-Type": "application/x-amz-json-1.1"
                 }
-        rsp = requests.post(auth_uri, headers=headers, json=data)
-        rsp = rsp.json()
+        rsp = requests.post(self.auth_url, headers=headers, json=data)
+        if rsp.status_code == 200:
+            rsp = rsp.json()
+        else:
+            raise RuntimeException(f"SRP auth error: {rsp.status_code}, {rsp.content}")
 
         assert rsp['ChallengeName']=="PASSWORD_VERIFIER"
         challenge = rsp['ChallengeParameters']
 
         user_id_for_srp = challenge['USER_ID_FOR_SRP']
-        usr.I = pool_id.split('_')[1]+user_id_for_srp
+        usr.I = self.pool_id.split('_')[1]+user_id_for_srp
 
         salt = challenge['SALT']
         srp_b = challenge['SRP_B']
@@ -196,13 +196,13 @@ class UserClient():
         hkdf = usr.process_challenge(bytes.fromhex(salt.zfill(32)), bytes.fromhex(srp_b.zfill(768)))
 
         secret_block_bytes = base64.standard_b64decode(secret_block)
-        msg = bytearray(pool_id.split('_')[1], 'utf-8') + bytearray(user_id_for_srp, 'utf-8') + \
+        msg = bytearray(self.pool_id.split('_')[1], 'utf-8') + bytearray(user_id_for_srp, 'utf-8') + \
             bytearray(secret_block_bytes) + bytearray(timestamp, 'utf-8')
         hmac_obj = hmac.new(hkdf, msg, digestmod=hashlib.sha256)
-        signature_string = base64.standard_b64encode(hmac_obj.digest())
+        signature_string = base64.standard_b64encode(hmac_obj.digest()).decode()
 
         data = {"ChallengeName": "PASSWORD_VERIFIER",
-                "ClientId": client_id,
+                "ClientId": self.client_id,
                 "ChallengeResponses": {"USERNAME": challenge['USERNAME'],
                     "TIMESTAMP": timestamp,
                     "PASSWORD_CLAIM_SECRET_BLOCK": secret_block,
@@ -211,33 +211,115 @@ class UserClient():
                 }
         headers["X-Amz-Target"] = "AWSCognitoIdentityProviderService.RespondToAuthChallenge"
 
-        rsp = requests.post(auth_uri, headers=headers, json=data)
-        return rsp.json()
+        rsp = requests.post(self.auth_url, headers=headers, json=data)
+        if rsp.status_code == 200:
+            return rsp.json()
+        else:
+            raise RuntimeException(f"SRP challenge error: {rsp.status_code}, {rsp.content}")
 
 
-    def _do_auth_srp_warrant(self, auth_uri, client_id, username, pool_id):
+    def _do_auth_srp_warrant(self):
         """this uses aws_srp.py from https://github.com/capless/warrant
         """
         from aws_srp import AWSSRP
-        aws = AWSSRP(username=username, password=getpass.getpass(), pool_id=pool_id,
-                             client_id=client_id, pool_region=pool_id.split('_')[0])
+        aws = AWSSRP(username=self.username, password=getpass.getpass(), pool_id=self.pool_id,
+                             client_id=self.client_id, pool_region=self.pool_id.split('_')[0])
         return aws.authenticate_user()
 
 
-    def _do_auth_password(self, auth_uri, client_id, username):
+    def _do_auth_password(self):
         data = {"AuthFlow": "USER_PASSWORD_AUTH",
-                "ClientId": client_id,
-                "AuthParameters": {"USERNAME":username, "PASSWORD":getpass.getpass()}
+                "ClientId": self.client_id,
+                "AuthParameters": {"USERNAME":self.username, "PASSWORD":getpass.getpass()}
                 }
         headers = {"X-Amz-Target": "AWSCognitoIdentityProviderService.InitiateAuth",
                 "Content-Type": "application/x-amz-json-1.1"
                 }
-        rsp = requests.post(auth_uri, headers=headers, json=data)
-        return rsp.json()
+        rsp = requests.post(self.auth_url, headers=headers, json=data)
+        if rsp.status_code == 200:
+            return rsp.json()
+        else:
+            raise RuntimeException(f"password authentication error: {rsp.status_code}, {rsp.content}")
+
+    def _auth_refresh(self, refresh_token):
+        data = {"AuthFlow": "REFRESH_TOKEN_AUTH",
+                "ClientId": self.client_id,
+                "AuthParameters": {"REFRESH_TOKEN": refresh_token}
+                }
+        headers = {"X-Amz-Target": "AWSCognitoIdentityProviderService.InitiateAuth",
+                "Content-Type": "application/x-amz-json-1.1"
+                }
+        rsp = requests.post(self.auth_url, headers=headers, json=data)
+        if rsp.status_code == 200:
+            return rsp.json()
+        else:
+            raise RuntimeException(f"token refresh error: {rsp.status_code}, {rsp.content}")
+
+
+class UserClient():
+    def __init__(self):
+        configpaths = [os.path.expanduser('~/.config/qick/config'),
+                '/etc/qick/config']
+
+        self.config = ConfigParser()
+        self.config.read(configpaths)
+
+
+        auth = CognitoAuth()
+        auth.client_id = self.config['service']['cognito_clientid']
+        auth.username = self.config['user']['username']
+        auth.auth_url = self.config['service']['cognito_url']
+        auth.pool_id = self.config['service']['cognito_userpool'] # only needed for SRP
+
+        self.api_url = self.config['service']['api_url']
+
+        self.session = requests.Session()
+        self.session.auth = auth
+
+    def add_user(self, email, fullname):
+        data = {
+                "Email": email,
+                "FullName": fullname
+                }
+        rsp = self.session.post(self.api_url + 'users', json=data)
+        if rsp.status_code == 200:
+            print("User successfully added! They should check their e-mail for a temporary password.")
+        else:
+            logging.warning(f"AddUser API error: {rsp.status_code}, {rsp.content}")
+
+    def add_device(self, device_name, refresh_timeout=60):
+        data = {
+                "DeviceName": device_name,
+                "RefreshTimeout": refresh_timeout
+                }
+        rsp = self.session.post(self.api_url + 'devices', json=data)
+        if rsp.status_code == 201:
+            rsp = rsp.json()
+            print("Device successfully added!")
+            print()
+            print("Put the following in the config file:")
+            print("[device]")
+            print(f"name = {rsp['DeviceName']}")
+            print(f"id = {rsp['DeviceId']}")
+            print()
+            print("Put the following in the device credentials file:")
+            print("[credentials]")
+            print(f"id = {rsp['ClientId']}")
+            print(f"secret = {rsp['ClientSecret']}")
+        else:
+            logging.warning(f"AddDevice API error: {rsp.status_code}, {rsp.content}")
+
+    def get_devices(self):
+        rsp = self.session.get(self.api_url + 'devices')
+        if rsp.status_code == 200:
+            return rsp.json()
+        else:
+            logging.warning(f"GetDevices API error: {rsp.status_code}, {rsp.content}")
+            return None
 
 
 if __name__ == "__main__":
-    logging.getLogger().setLevel(logging.INFO)
+    logging.getLogger().setLevel(logging.WARNING)
 
     client = UserClient()
     Fire(client)
