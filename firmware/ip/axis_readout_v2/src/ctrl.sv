9435713e74c78c526be5aefb7ef5f3ea29a44aca
Leandro Stefanazzi <lstefana@fnal.gov>
2023-02-03 10:00:04 -0600

    modified pipeline of readout_v2 to run faster

--- firmware/ip/axis_readout_v2/src/ctrl.sv
+++ firmware/ip/axis_readout_v2/src/ctrl.sv
@@ -57,16 +57,24 @@ reg		[31:0]	cnt_n_reg;
 // Pinc/phase.
 wire	[31:0]	pinc_int;
 reg		[31:0]	pinc_r1;
+reg		[31:0]	pinc_r2;
 wire	[31:0]	pinc_N;
 reg		[31:0]	pinc_N_r1;
 reg		[31:0]	pinc_N_r2;
 reg		[31:0]	pinc_N_r3;
+reg		[31:0]	pinc_N_r4;
+reg		[31:0]	pinc_N_r5;
 wire	[31:0]	pinc_Nm;
 reg		[31:0]	pinc_Nm_r1;
+reg		[31:0]	pinc_Nm_r2;
+reg		[31:0]	pinc_Nm_r3;
 
 wire	[31:0]	phase_int;
 reg		[31:0]	phase_r1; 
 reg		[31:0]	phase_r2; 
+reg		[31:0]	phase_r3; 
+reg		[31:0]	phase_r4; 
+reg		[31:0]	phase_r5; 
 wire	[31:0]	phase_0;
 reg		[31:0]	phase_0_r1;
 
@@ -74,15 +82,10 @@ reg		[31:0]	phase_0_r1;
 wire	[31:0]	phase_v0 	[0:N_DDS-1];
 reg		[31:0]	phase_v0_r1 [0:N_DDS-1];
 reg		[31:0]	phase_v0_r2 [0:N_DDS-1];
+reg		[31:0]	phase_v0_r3 [0:N_DDS-1];
+reg		[31:0]	phase_v0_r4 [0:N_DDS-1];
 wire	[31:0]	phase_v1 	[0:N_DDS-1];
 reg		[31:0]	phase_v1_r1 [0:N_DDS-1];
-
-// sync.
-reg				sync_reg;
-reg				sync_reg_r1;
-reg				sync_reg_r2;
-reg				sync_reg_r3;
-reg				sync_reg_r4;
 
 // Number of samples.
 wire	[15:0]	nsamp_int;
@@ -93,18 +96,26 @@ reg		[1:0]	outsel_r1;
 reg		[1:0]	outsel_r2;
 reg		[1:0]	outsel_r3;
 reg		[1:0]	outsel_r4;
+reg		[1:0]	outsel_r5;
+reg		[1:0]	outsel_r6;
+reg		[1:0]	outsel_r7;
 
 // Mode.
 wire			mode_int;
 
 // Load enable flag.
 wire			load_int;
-reg				load_r;
+reg				load_r1;
+reg				load_r2;
+reg				load_r3;
+reg				load_r4;
+reg				load_r5;
+reg				load_r6;
+reg				load_r7;
+reg				load_r8;
 
 // Fifo Read Enable.
 reg			    rd_en_int;
-reg				rd_en_r1;
-reg				rd_en_r2;
 
 // Counter.
 reg		[15:0]	cnt;
@@ -124,33 +135,41 @@ always @(posedge clk) begin
 
 		// Pinc/phase/sync.
 		pinc_r1			<= 0;
+		pinc_r2			<= 0;
 		pinc_N_r1		<= 0;
 		pinc_N_r2		<= 0;
 		pinc_N_r3		<= 0;
+		pinc_N_r4		<= 0;
+		pinc_N_r5		<= 0;
 		pinc_Nm_r1		<= 0;
+		pinc_Nm_r2		<= 0;
+		pinc_Nm_r3		<= 0;
 
 		phase_r1		<= 0;
 		phase_r2		<= 0;
+		phase_r3		<= 0;
+		phase_r4		<= 0;
+		phase_r5		<= 0;
 		phase_0_r1		<= 0;
-
-		sync_reg		<= 0;
-		sync_reg_r1		<= 0;
-		sync_reg_r2		<= 0;
-		sync_reg_r3		<= 0;
-		sync_reg_r4		<= 0;
 
 		// Output selection.
 		outsel_r1		<= 0;
 		outsel_r2		<= 0;
 		outsel_r3		<= 0;
 		outsel_r4		<= 0;
+		outsel_r5		<= 0;
+		outsel_r6		<= 0;
+		outsel_r7		<= 0;
 
 		// Load enable flag.
-		load_r			<= 0;
-
-		// Fifo Read Enable.
-		rd_en_r1		<= 0;
-		rd_en_r2		<= 0;
+		load_r1			<= 0;
+		load_r2			<= 0;
+		load_r3			<= 0;
+		load_r4			<= 0;
+		load_r5			<= 0;
+		load_r6			<= 0;
+		load_r7			<= 0;
+		load_r8			<= 0;
 
 		// Counter.
 		cnt				<= 0;
@@ -167,43 +186,51 @@ always @(posedge clk) begin
 		endcase
 
 		// Fifo dout register.
-		if (load_r)
+		if (load_r1)
 			fifo_dout_r	<= fifo_dout_i;
 
 		// Non-stop counter for time calculation.
 		cnt_n			<= cnt_n + N_DDS;
-		if (sync_reg)
+		if (load_r2)
 			cnt_n_reg	<= cnt_n;
 
 		// Pinc/phase/sync.
 		pinc_r1			<= pinc_int;
+		pinc_r2			<= pinc_r1;
 		pinc_N_r1		<= pinc_N;
 		pinc_N_r2		<= pinc_N_r1;
 		pinc_N_r3		<= pinc_N_r2;
+		pinc_N_r4		<= pinc_N_r3;
+		pinc_N_r5		<= pinc_N_r4;
 		pinc_Nm_r1		<= pinc_Nm;
+		pinc_Nm_r2		<= pinc_Nm_r1;
+		pinc_Nm_r3		<= pinc_Nm_r2;
 
 		phase_r1		<= phase_int;
 		phase_r2		<= phase_r1;
+		phase_r3		<= phase_r2;
+		phase_r4		<= phase_r3;
+		phase_r5		<= phase_r4;
 		phase_0_r1		<= phase_0;
-
-		sync_reg		<= load_r;
-		sync_reg_r1		<= sync_reg;
-		sync_reg_r2		<= sync_reg_r1;
-		sync_reg_r3		<= sync_reg_r2;
-		sync_reg_r4		<= sync_reg_r3;
 
 		// Output selection.
 		outsel_r1		<= outsel_int;
 		outsel_r2		<= outsel_r1;
 		outsel_r3		<= outsel_r2;
 		outsel_r4		<= outsel_r3;
+		outsel_r5		<= outsel_r4;
+		outsel_r6		<= outsel_r5;
+		outsel_r7		<= outsel_r6;
 
 		// Load enable flag.
-		load_r			<= load_int;
-
-		// Fifo Read Enable.
-		rd_en_r1		<= rd_en_int;
-		rd_en_r2		<= rd_en_r1;
+		load_r1			<= load_int;
+		load_r2			<= load_r1;
+		load_r3			<= load_r2;
+		load_r4			<= load_r3;
+		load_r5			<= load_r4;
+		load_r6			<= load_r5;
+		load_r7			<= load_r6;
+		load_r8			<= load_r7;
 
 		// Counter.
 		if (rd_en_int)
@@ -235,11 +262,11 @@ assign outsel_int	= fifo_dout_r[81:80];
 assign mode_int		= fifo_dout_r[82];
 
 // Frequency calculation.
-assign pinc_N		= pinc_r1*N_DDS;
+assign pinc_N		= pinc_r2*N_DDS;
 
 // Phase calculation.
-assign pinc_Nm		= pinc_r1*cnt_n_reg;
-assign phase_0		= pinc_Nm_r1 + phase_r2;
+assign pinc_Nm		= pinc_r2*cnt_n_reg;
+assign phase_0		= pinc_Nm_r3 + phase_r5;
 
 // Phase vectors.
 generate
@@ -251,6 +278,8 @@ genvar i;
 				// v0.
 				phase_v0_r1[i]	<= 0;	
 				phase_v0_r2[i]	<= 0;	
+				phase_v0_r3[i]	<= 0;	
+				phase_v0_r4[i]	<= 0;	
 
 				// v1.
 				phase_v1_r1[i]	<= 0;
@@ -259,6 +288,8 @@ genvar i;
 				// v0.
 				phase_v0_r1[i] 	<= phase_v0[i];	
 				phase_v0_r2[i] 	<= phase_v0_r1[i];	
+				phase_v0_r3[i] 	<= phase_v0_r2[i];	
+				phase_v0_r4[i] 	<= phase_v0_r3[i];	
 
 				// v1.
 				phase_v1_r1[i]	<= phase_v1[i];
@@ -266,13 +297,13 @@ genvar i;
 		end
 
 		// v0.
-		assign phase_v0[i] 		= pinc_r1*i;
+		assign phase_v0[i] 		= pinc_r2*i;
 
 		// v1.
-		assign phase_v1[i] 		= phase_v0_r2[i] + phase_0_r1;
+		assign phase_v1[i] 		= phase_v0_r4[i] + phase_0_r1;
 
 		// dds_ctrl_o output.
-		assign dds_ctrl_o[i*72 +: 72]	= {7'h00,sync_reg_r4,phase_v1_r1[i],pinc_N_r3};
+		assign dds_ctrl_o[i*72 +: 72]	= {7'h00,load_r8,phase_v1_r1[i],pinc_N_r5};
 	end
 endgenerate
 
@@ -281,7 +312,7 @@ assign load_int 	= rd_en_int & ~fifo_empty_i;
 
 // Assign outputs.
 assign fifo_rd_en_o	= rd_en_int;
-assign outsel_o		= outsel_r4;
+assign outsel_o		= outsel_r7;
 
 endmodule
 
