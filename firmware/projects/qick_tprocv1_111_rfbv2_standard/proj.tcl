40f4102419cf640e18eadb47e811e91f38b4b459
Sho Uemura <suemura@fnal.gov>
2024-09-02 17:40:54 -0500

    reorganize project files

--- firmware/projects/qick_tprocv1_111_rfbv2_standard/proj.tcl
+++ firmware/projects/qick_tprocv1_111_rfbv2_standard/proj.tcl
@@ -7,7 +7,7 @@ if { [info exists ::origin_dir_loc] } {
 }
 
 # Set the project name
-set _xil_proj_name_ "top_111"
+set _xil_proj_name_ "top"
 
 # Set the directory path for the original project from where this script was exported
 set orig_proj_dir "[file normalize "$origin_dir/"]"
@@ -21,10 +21,9 @@ set proj_dir [get_property directory [current_project]]
 # Set project properties
 set obj [current_project]
 set_property -name "board_part" -value "xilinx.com:zcu111:part0:1.4" -objects $obj
-set_property -name "corecontainer.enable" -value "1" -objects $obj
 set_property -name "default_lib" -value "xil_defaultlib" -objects $obj
-set_property -name "enable_core_container" -value "1" -objects $obj
-set_property -name "enable_resource_estimation" -value "0" -objects $obj
+set_property -name "dsa.accelerator_binary_content" -value "bitstream" -objects $obj
+set_property -name "dsa.accelerator_binary_format" -value "xclbin2" -objects $obj
 set_property -name "dsa.board_id" -value "zcu111" -objects $obj
 set_property -name "dsa.description" -value "Vivado generated DSA" -objects $obj
 set_property -name "dsa.dr_bd_base_address" -value "0" -objects $obj
@@ -42,8 +41,6 @@ set_property -name "enable_vhdl_2008" -value "1" -objects $obj
 set_property -name "ip_cache_permissions" -value "read write" -objects $obj
 set_property -name "ip_output_repo" -value "$proj_dir/${_xil_proj_name_}.cache/ip" -objects $obj
 set_property -name "mem.enable_memory_map_generation" -value "1" -objects $obj
-set_property -name "platform.board_id" -value "zcu111" -objects $obj
-set_property -name "revised_directory_structure" -value "1" -objects $obj
 set_property -name "sim.central_dir" -value "$proj_dir/${_xil_proj_name_}.ip_user_files" -objects $obj
 set_property -name "sim.ip.auto_export_scripts" -value "1" -objects $obj
 set_property -name "simulator_language" -value "Mixed" -objects $obj
@@ -51,7 +48,7 @@ set_property -name "xpm_libraries" -value "XPM_CDC XPM_FIFO XPM_MEMORY" -objects
 
 # Set IP repository paths
 set obj [get_filesets sources_1]
-set_property "ip_repo_paths" "[file normalize "$origin_dir/ip"]" $obj
+set_property "ip_repo_paths" "[file normalize "$origin_dir/../../ip"]" $obj
 
 # Rebuild user ip_repo's index before adding any source files
 update_ip_catalog -rebuild
@@ -59,7 +56,8 @@ update_ip_catalog -rebuild
 # Set 'sources_1' fileset object
 set obj [get_filesets sources_1]
 set files [list \
-	[ file normalize "$origin_dir/hdl/vect2bits_16.v"]	\
+	[ file normalize "$origin_dir/../../hdl/vect2bits_16.v"]	\
+	[ file normalize "$origin_dir/../../hdl/lo_spi_mux_v2.vhd"]	\
 ]
 add_files -norecurse -fileset $obj $files
 
@@ -68,13 +66,13 @@ set obj [get_filesets constrs_1]
 
 # Add/Import constrs file and set constrs file properties
 set files [list \
-	[ file normalize "$origin_dir/xdc/timing_111.xdc"] 	\
-	[ file normalize "$origin_dir/xdc/ios_111.xdc"] 	\
+	[ file normalize "$origin_dir/timing.xdc"] 	\
+	[ file normalize "$origin_dir/ios.xdc"] 	\
 ]
 add_files -fileset $obj $files
 
 # Source Block Design.
-set file "[file normalize "$origin_dir/bd/bd_111_2022-1.tcl"]"
+set file "[file normalize "$origin_dir/bd_2020-2.tcl"]"
 source $file
 
 # Update compile order.
@@ -88,8 +86,6 @@ make_wrapper -files [get_files d_1.bd] -top
 
 # Add files to sources_1 fileset
 set files [list \
-  [file normalize "${origin_dir}/top_111/top_111.srcs/sources_1/bd/d_1/hdl/d_1_wrapper.v" ]\
+  [file normalize "${origin_dir}/top/top.srcs/sources_1/bd/d_1/hdl/d_1_wrapper.v" ]\
 ]
 add_files -fileset $obj $files
-
-set_property strategy "Performance_Explore" [get_runs impl_1]
