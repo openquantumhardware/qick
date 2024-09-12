49c41736b2a6cdecdf8cd045f9961f289626343e
Sho Uemura <suemura@fnal.gov>
2024-09-11 19:03:37 -0500

    add pyro remote traceback code to make_proxy; make start_server host IP detection work with static IPs

--- qick_lib/qick/pyro.py
+++ qick_lib/qick/pyro.py
@@ -27,7 +27,7 @@ def start_nameserver(ns_host='0.0.0.0', ns_port=8888):
     Pyro4.config.PICKLE_PROTOCOL_VERSION=4
     Pyro4.naming.startNSloop(host=ns_host, port=ns_port)
 
-def start_server(ns_host, ns_port=8888, proxy_name='myqick', soc_class=QickSoc, **kwargs):
+def start_server(ns_host, ns_port=8888, proxy_name='myqick', soc_class=QickSoc, iface='eth0', **kwargs):
     """Initializes the QickSoc and starts a Pyro4 proxy server.
 
     Parameters
@@ -42,6 +42,8 @@ def start_server(ns_host, ns_port=8888, proxy_name='myqick', soc_class=QickSoc, 
         multiple boards can use the same nameserver, but must have different names
     soc_class : class
         class to proxy, if you want to use a class other than QickSoc (e.g. if you need to use RFQickSoc)
+    iface : str
+        network interface whose address will be advertised - this should be the interface you expect to make Pyro connections on
     kwargs : optional named arguments
         any other options will be passed to the QickSoc constructor;
         see QickSoc documentation for details
@@ -62,10 +64,16 @@ def start_server(ns_host, ns_port=8888, proxy_name='myqick', soc_class=QickSoc, 
     host = Pyro4.socketutil.getInterfaceAddress(ns._pyroUri.host)
     # if the nameserver is running on the QICK, the above will usually return the loopback address - not useful
     if host=="127.0.0.1":
-        # get the IPv4 address of the eth0 interface
+        # if the eth0 interface has an IPv4 address, use that
+        # otherwise use the address of any interface starting with "eth0" (e.g. eth0:1, which is typically a static IP)
         # unless you have an unusual network config (e.g. VPN), this is the interface clients will want to connect to
-        (myaddr,) = [addr for addr in psutil.net_if_addrs()['eth0'] if addr.family==socket.AddressFamily.AF_INET]
-        host = myaddr.address
+        for name, addrs in psutil.net_if_addrs().items():
+            addrs_v4 = [addr.address for addr in addrs if addr.family==socket.AddressFamily.AF_INET]
+            if len(addrs_v4)==1:
+                if name.startswith(iface):
+                    host = addrs_v4[0]
+                if name==iface:
+                    break
     daemon = Pyro4.Daemon(host=host)
 
     # if you want to use a different firmware image or set some initialization options, you would do that here
@@ -88,7 +96,7 @@ def start_server(ns_host, ns_port=8888, proxy_name='myqick', soc_class=QickSoc, 
     print("starting daemon")
     daemon.requestLoop() # this will run forever until interrupted
 
-def make_proxy(ns_host, ns_port=8888, proxy_name='myqick'):
+def make_proxy(ns_host, ns_port=8888, proxy_name='myqick', remote_traceback=True):
     """Connects to a QickSoc proxy server.
 
     Parameters
@@ -100,6 +108,8 @@ def make_proxy(ns_host, ns_port=8888, proxy_name='myqick'):
         the port number you used when starting the nameserver
     proxy_name : str
         name for the QickSoc proxy you used when running start_server()
+    remote_traceback : bool
+        if running in IPython (Jupyter etc.), reconfigure the IPython exception handler to print the remote Pyro traceback
 
     Returns
     -------
@@ -119,4 +129,19 @@ def make_proxy(ns_host, ns_port=8888, proxy_name='myqick'):
 
     soc = Pyro4.Proxy(ns.lookup(proxy_name))
     soccfg = QickConfig(soc.get_cfg())
+
+    # adapted from https://pyro4.readthedocs.io/en/stable/errors.html and https://stackoverflow.com/a/70433500
+    if remote_traceback:
+        try:
+            import IPython
+            import sys
+            ip = IPython.get_ipython()
+            if ip is not None:
+                def exception_handler(self, etype, evalue, tb, tb_offset=None):
+                    sys.stderr.write("".join(Pyro4.util.getPyroTraceback()))
+                    # self.showtraceback((etype, evalue, tb), tb_offset=tb_offset)  # standard IPython's printout
+                ip.set_custom_exc((Exception,), exception_handler)  # register your handler
+        except Exception as e:
+            raise RuntimeError("Failed to set up Pyro exception handler: ", e)
+
     return(soc, soccfg)
