d756a015cdef7c01899259416d0f0c50ae74f4f5
Sho Uemura <suemura@fnal.gov>
2024-12-23 21:33:08 -0600

    one more data file

--- setup.py
+++ setup.py
@@ -179,6 +179,7 @@ setup(
     package_data={  # Optional
         'qick': ['*.bit', '*.hwh', 'VERSION',
                  'ipq_pynq_utils/ipq_pynq_utils/data/*.json',
+                 'ipq_pynq_utils/ipq_pynq_utils/data/*.txt',
                  'ipq_pynq_utils/ipq_pynq_utils/data/*.h',
                  'ipq_pynq_utils/ipq_pynq_utils/data/clockFiles/*.txt',
                  ]
