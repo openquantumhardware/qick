42a01ba06cf2cfaaf77720b12d2f11f2649ff96a
Horacio Arnaldi <lharnaldi@gmail.com>
2025-02-03 18:05:02 +0100

    Modify the mux2 to add parameter

--- cores/mux2/rtl/mux2.sv
+++ cores/mux2/rtl/mux2.sv
@@ -1,12 +1,11 @@
 module mux2 #(
-  parameter WIDTH = 8
-  )(
-    input logic [WIDTH-1:0] d0, d1,
-    input logic s,
-    output logic [WIDTH-1:0] y
-    );
+  parameter NB_DATA = 8
+)(
+  input logic [NB_DATA-1:0] i_d0, i_d1,
+  input logic i_s,
+  output logic [NB_DATA-1:0] o_y
+  );
  
-  assign y = s ? d1 : d0;
+  assign o_y = i_s ? i_d1 : i_d0;
   
 endmodule
-
