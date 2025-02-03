4716c64b24399d67b86b9ffd37608c063fdd5132
Horacio Arnaldi <lharnaldi@gmail.com>
2025-02-03 18:02:35 +0100

    Add the fuse_zynq library in the libraries

--- scripts/utils/build_fusesocenv.sh
+++ scripts/utils/build_fusesocenv.sh
@@ -10,6 +10,9 @@ source venv/bin/activate
 # simulation libraries
 fusesoc library add --location $PWD/.fusesoc_libraries/svunit_lib svunit_lib https://github.com/ivanvig/svunit.git
 
+#zynq building library
+fusesoc library add --locatio .fusesoc_libraries/fuse-zynq fuse-zynq https://github.com/craigjb/fuse-zynq
+
 # add cores from root 
 fusesoc library add --location .fusesoc_libraries/pynq_fused pynq_fused .
 
