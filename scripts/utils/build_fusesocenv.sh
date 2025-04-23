152c32a63e3d6f6023b5ebad2b29887f75523031
Luis Arnaldi <lharnaldi@gmail.com>
2025-04-23 10:36:54 -0500

    Remove a typo in the build_fusesoc.sh script

--- scripts/utils/build_fusesocenv.sh
+++ scripts/utils/build_fusesocenv.sh
@@ -8,10 +8,10 @@ source venv/bin/activate
 # Add libraries
 
 # simulation libraries
-fusesoc library add --location $PWD/.fusesoc_libraries/svunit_lib svunit_lib https://github.com/ivanvig/svunit.git
+fusesoc library add --location .fusesoc_libraries/svunit_lib svunit_lib https://github.com/ivanvig/svunit.git
 
 #zynq building library
-fusesoc library add --locatio .fusesoc_libraries/fuse-zynq fuse-zynq https://github.com/craigjb/fuse-zynq
+fusesoc library add --location .fusesoc_libraries/fuse-zynq fuse-zynq https://github.com/craigjb/fuse-zynq
 
 # add cores from root 
 fusesoc library add --location .fusesoc_libraries/pynq_fused pynq_fused .
