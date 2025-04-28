9bfd131023b6fb5dc43cc2c557440482f030c13e
Horacio Arnaldi <lharnaldi@gmail.com>
2025-04-27 22:08:00 -0500

    Change the name of the data port in sync_n core

--- cores/sync_n/rtl/sync_n.sv
+++ cores/sync_n/rtl/sync_n.sv
@@ -1,10 +1,26 @@
+///////////////////////////////////////////////////////////////////////////////
+// vim:set shiftwidth=3 softtabstop=3 expandtab:
+//
+// Fermi Fordward Alliance LLC
+//
+// Module: sync_n.sv
+// Project: QICK
+// Description: Synchronization registers. It serves as clock 
+//              domain crossing logic. Minimum value of N=2.
+//              It is a parametrizable shift register. 
+//              Basic CDC can be accomplished setting N=2
+//
+//
+// Change history: 04/27/25 - Created by @lharnaldi
+//
+///////////////////////////////////////////////////////////////////////////////
 module sync_n #(
     parameter N = 8 //number of stages
 )(
     input logic  i_clk ,
     input logic  i_rstn,
-    input logic  i_d   ,
-    output logic o_d
+    input logic  i_data,
+    output logic o_data
 );
 
     logic [N-1:0] d_r;
@@ -18,8 +34,8 @@ module sync_n #(
      end
     end
 
-    assign d_n = {d_r[N-2:0],i_d};
-    assign o_d = d_r[N-1];
+    assign d_n    = {d_r[N-2:0],i_data};
+    assign o_data = d_r[N-1];
 
 endmodule
 
