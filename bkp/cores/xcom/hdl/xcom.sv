1b8cb604f144f8d92434c47d4a42e794d8639186
Luis Arnaldi <lharnaldi@gmail.com>
2025-05-14 16:28:31 -0500

    Working in the final integration of the xcom core. Now doing the synchronization between clock domains

--- bkp/cores/xcom/hdl/xcom.sv
+++ bkp/cores/xcom/hdl/xcom.sv
@@ -5,76 +5,109 @@
 //
 // Module: xcom.sv
 // Project: QICK 
-// Description: Board communication peripheral
+// Description: 
+// Transmitter and Receiver interface for the XCOM block. 
+// 
+//Inputs:
+// - i_clk      clock signal
+// - i_rstn     active low reset signal
+// - i_sync     synchronization signal. Lets the XCOM synchronize with an
+//              external signal. Actuates in coordination with the 
+//              XCOM_QRST_SYNC command.
+// - i_cfg_tick this input is connected to the AXI_CFG register and 
+//              determines the duration of the xcom_clk output signal.
+//              xcom_clk will be either in state 1 or 0 for CFG_AXI clock 
+//              cycles (i_clk). Possible values ranges from 0 to 7 with 
+//              0 equal to two clock cycles and 7 equal to 15 clock 
+//              cycles. As an example, if i_cfg_tick = 2 and 
+//              i_clk = 500 MHz, then xcom_clk would be ~125 MHz.
+// - i_req_net      transmission requirement signal. Signal indicating a new
+//              data transmission starts.  
+// - i header   this is the header to be sent to the slaves. 
+//              bit 7      is sometimes used to indicate a 
+//                         synchronization in other places in the 
+//                         XCOM hierarchy
+//              bits [6:5] determines the data length to transmit:
+//                         00 no data
+//                         01 8-bit data
+//                         10 16-bit data
+//                         11 32-bit data
+//              bit 4      not used in this block
+//              bits [3:0] not used in this block. Sometimes used 
+//                         as mem_id and sometimes used as board 
+//                         ID in the XCOM hierarchy 
+// - i_data     the data to be transmitted 
+//Outputs:
+// - o_ready    signal indicating the ip is ready to receive new data to
+//              transmit
+// - o_data     serial data transmitted. This is the general output of the
+//              XCOM block
+// - o_clk      serial clock for transmission. This is the general output of
+//              the XCOM block
+// - o_dbg_state debug port for monitoring the state of the internal FSM
 //
-//
-// Change history: 10/20/24 - v2 Started by mdifederico
-//                 10/29/04 - Made the RESET signal last longer than
-//                 a single clock
-//                 01/08/05 - dma_rd_mac is now an input as round-robin
-//                 lookup is used
-//                 01/13/05 - split dma_intr into read and write sigs
+// Change history: 10/20/24 - v2 Started by @mdifederico
+//                 05/13/25 - Refactored by @lharnaldi
+//                          - the sync_n core was removed to sync all signals
+//                            in one place (external).
 //
 ///////////////////////////////////////////////////////////////////////////////
-
-module xcom # (
+module xcom import qick_pkg::*;
+# (
    parameter CH           = 2 ,
    parameter SYNC         = 1 ,
    parameter DEBUG        = 1
 )(
-// CLK & RST
-   input  logic             ps_clk        ,
-   input  logic             ps_aresetn    ,
-   input  logic             c_clk         ,
-   input  logic             c_aresetn     ,
-   input  logic             t_clk         ,
-   input  logic             t_aresetn     ,
-   input  logic             x_clk         ,
-   input  logic             x_aresetn     ,
-// QICK PERIPHERAL INTERFACE (c_clk)
-   input  logic             qp_en_i       , 
-   input  logic  [4:0]      qp_op_i       , 
-   input  logic  [31:0]     qp_dt1_i      , 
-   input  logic  [31:0]     qp_dt2_i      , 
-   output logic              qp_rdy_o      , 
-   output logic   [31:0]     qp_dt1_o      , 
-   output logic   [31:0]     qp_dt2_o      , 
-   output logic              qp_vld_o      , 
-   output logic              qp_flag_o     , 
+   input  logic             i_ps_clk        ,
+   input  logic             i_ps_rstn    ,
+   input  logic             i_core_clk         ,
+   input  logic             i_core_rstn     ,
+   input  logic             i_time_clk         ,
+   input  logic             i_time_rstn     ,
+// QICK PERIPHERAL INTERFACE (i_core_clk)
+   input  logic             i_qp_en       , 
+   input  logic  [5-1:0]    i_qp_op       , 
+   input  logic  [32-1:0]   i_qp_data1      , 
+   input  logic  [32-1:0]   i_qp_data2      , 
+   output logic             o_qp_ready      , 
+   output logic   [32-1:0]  o_qp_data1      , 
+   output logic   [32-1:0]  o_qp_data2      , 
+   output logic             o_qp_valid      , 
+   output logic             o_qp_flag     , 
 // Qick CONTROL
-   input  logic             pulse_sync_i  ,
-   output logic             proc_start_o  ,
-   output logic             proc_stop_o   ,
-   output logic             time_rst_o    ,
-   output logic             time_updt_o   ,
-   output logic  [31:0]     time_updt_dt_o,
-   output logic             core_start_o  ,
-   output logic             core_stop_o   ,
+   input  logic             i_sync  ,
+   output logic             o_proc_start  ,
+   output logic             o_proc_stop   ,
+   output logic             o_time_rst    ,
+   output logic             o_time_update   ,
+   output logic  [32-1:0]   o_time_update_data,
+   output logic             o_core_start  ,
+   output logic             o_core_stop   ,
 // XCOM 
-   output logic  [ 3:0]     xcom_id_o     ,
-// IO XCOM (x_clk)
-   input  logic  [CH-1:0]   xcom_ck_i     ,
-   input  logic  [CH-1:0]   xcom_dt_i     ,
-   output logic             xcom_ck_o     ,
-   output logic             xcom_dt_o     ,
-// AXI-Lite DATA Slave I/F (ps_clk)
-   input  logic [5:0]       s_axi_awaddr  ,
-   input  logic [2:0]       s_axi_awprot  ,
+   output logic  [ 4-1:0]   o_xcom_id     ,
+// IO XCOM (i_time_clk)
+   input  logic  [CH-1:0]   i_xcom_clk     ,
+   input  logic  [CH-1:0]   i_xcom_data     ,
+   output logic             o_xcom_clk     ,
+   output logic             o_xcom_data     ,
+// AXI-Lite DATA Slave I/F (i_ps_clk)
+   input  logic [6-1:0]     s_axi_awaddr  ,
+   input  logic [3-1:0]     s_axi_awprot  ,
    input  logic             s_axi_awvalid ,
    output logic             s_axi_awready ,
-   input  logic [31:0]      s_axi_wdata   ,
-   input  logic [ 3:0]      s_axi_wstrb   ,
+   input  logic [32-1:0]    s_axi_wdata   ,
+   input  logic [ 4-1:0]    s_axi_wstrb   ,
    input  logic             s_axi_wvalid  ,
    output logic             s_axi_wready  ,
-   output logic [ 1:0]      s_axi_bresp   ,
+   output logic [ 2-1:0]    s_axi_bresp   ,
    output logic             s_axi_bvalid  ,
    input  logic             s_axi_bready  ,
-   input  logic [ 5:0]      s_axi_araddr  ,
-   input  logic [ 2:0]      s_axi_arprot  ,
+   input  logic [ 6-1:0]    s_axi_araddr  ,
+   input  logic [ 3-1:0]    s_axi_arprot  ,
    input  logic             s_axi_arvalid ,
    output logic             s_axi_arready ,
-   output logic [31:0]      s_axi_rdata   ,
-   output logic [ 1:0]      s_axi_rresp   ,
+   output logic [32-1:0]    s_axi_rdata   ,
+   output logic [ 2-1:0]    s_axi_rresp   ,
    output logic             s_axi_rvalid  ,
    input  logic             s_axi_rready        
 );
@@ -83,44 +116,47 @@ module xcom # (
 ///////////////////////////////////////////////////////////////////////////////
 
 // XCOM Control (From Python and tProc)
-logic [ 7:0] cmd_op ;
-logic [31:0] cmd_dt ;
-logic [ 7:0] cmd_cnt_ds ;
+logic [ 8-1:0] cmd_op ;
+logic [32-1:0] cmd_dt ;
+logic [ 8-1:0] cmd_cnt_ds ;
 
-logic [ 5:0] XCOM_CTRL ;
-logic [ 2:0] XCOM_CFG ;
-logic [31:0] AXI_DT1, AXI_DT2 ;
-logic [ 3:0] AXI_ADDR ;
+logic [32-1:0] s_xcom_ctrl ;//6
+logic [6-1:0]  s_xcom_ctrl_sync ;//6
+logic [32-1:0] s_xcom_cfg ;
+logic [4-1:0]  s_xcom_cfg_sync ;
+logic [4-1:0]  xcom_cfg;
+logic [32-1:0] s_axi_data1;
+logic [32-1:0] s_axi_data1_sync;
+logic [32-1:0] s_axi_data2 ;
+logic [32-1:0] s_axi_data2_sync;
+logic [ 4-1:0] s_axi_addr ;
 
+logic [ 6-1:0] p_ctrl  ; 
+logic [32-1:0] p_dt [2]; 
+logic [32-1:0] c_dt [2]; 
 
+logic [32-1:0] xcom_mem_data [15];
+logic [32-1:0] axi_mem_dt;
 
-logic [ 5:0] p_ctrl  ; 
-logic [31:0] p_dt [2]; 
-logic [31:0] c_dt [2]; 
+logic [32-1:0] xreg_debug;
+logic [29-1:0] xreg_status;
+logic [32-1:0] xcom_rx_ds ;
+logic [32-1:0] xcom_tx_ds ;
+logic [21-1:0] xcom_status_ds ;
+logic [32-1:0] xcom_debug_ds  ;
 
-logic [31:0] xcom_mem_dt [15];
-logic [31:0] axi_mem_dt;
-
-logic [31:0] xreg_debug;
-logic [28:0] xreg_status;
-logic [31:0]      xcom_rx_ds ;
-logic [31:0]      xcom_tx_ds ;
-logic [20:0]      xcom_status_ds ;
-logic [31:0]      xcom_debug_ds  ;
-
-assign p_ctrl = XCOM_CTRL;
-assign c_dt   = '{qp_dt1_i, qp_dt2_i};
-assign p_dt   = '{AXI_DT1  , AXI_DT2};
+assign p_ctrl = s_xcom_ctrl;
+assign c_dt   = '{i_qp_data1, i_qp_data2};
 
 qick_xcom_cmd QICK_CMD(
-   .ps_clk_i      ( ps_clk      ),
-   .ps_rst_ni     ( ps_aresetn  ),
-   .c_clk_i       ( c_clk       ),
-   .c_rst_ni      ( c_aresetn   ),
-   .x_clk_i       ( x_clk       ),
-   .x_rst_ni      ( x_aresetn   ),
-   .c_en_i        ( qp_en_i     ),
-   .c_op_i        ( qp_op_i     ),
+   .ps_clk_i      ( i_ps_clk      ),
+   .ps_rst_ni     ( i_ps_rstn  ),
+   .c_clk_i       ( i_core_clk       ),
+   .c_rst_ni      ( i_core_rstn   ),
+   .x_clk_i       ( i_time_clk       ),
+   .x_rst_ni      ( i_time_rstn   ),
+   .c_en_i        ( i_qp_en     ),
+   .c_op_i        ( i_qp_op     ),
    .c_dt_i        ( c_dt        ),
    .p_ctrl_i      ( p_ctrl      ),
    .p_dt_i        ( p_dt        ),
@@ -132,44 +168,48 @@ qick_xcom_cmd QICK_CMD(
    .cmd_dt_o      ( cmd_dt      ),
    .cmd_cnt_do    ( cmd_cnt_ds     ));
 
-logic [3:0] xcom_cfg;
-assign   xcom_cfg = {XCOM_CFG[2:0]+1'b1, 1'b0};
+xcom_cmd u_xcom_cmd(
+   .i_clk            ( i_time_clk         ),
+   .i_rstn           ( i_time_rstn        ),
+   .i_tproc_en       ( tb_i_tproc_en  ),
+   .i_tproc_op       ( tb_i_tproc_op  ),
+   .i_tproc_data     ( s_tproc_data   ),
+   .i_ps_ctrl        ( tb_i_ps_ctrl   ),
+   .i_ps_data        ( s_ps_data      ), 
+   .o_req_loc        ( tb_o_req_loc   ),
+   .i_ack_loc        ( tb_i_ack_loc   ),
+   .o_req_net        ( tb_o_req_net   ),
+   .i_ack_net        ( tb_i_ack_net   ),
+   .o_op             ( tb_o_op        ),
+   .o_data           ( tb_o_data      ),
+   .o_data_cntr      ( tb_o_data_cntr )
+   );
+
 
 qick_xcom # (
    .CH    ( CH   ),
    .SYNC  ( SYNC )
 ) XCOM (
-   .c_clk_i        ( c_clk          ),
-   .c_rst_ni       ( c_aresetn      ),
-   .t_clk_i        ( t_clk          ),
-   .t_rst_ni       ( t_aresetn      ),
-   .x_clk_i        ( x_clk          ),
-   .x_rst_ni       ( x_aresetn      ),
-   .pulse_sync_i   ( pulse_sync_i   ),
    .cmd_loc_req_i  ( cmd_loc_req    ),
    .cmd_loc_ack_o  ( cmd_loc_ack    ),
    .cmd_net_req_i  ( cmd_net_req    ),
    .cmd_net_ack_o  ( cmd_net_ack    ),
    .cmd_op_i       ( cmd_op         ),
    .cmd_dt_i       ( cmd_dt         ),
-   .qp_rdy_o       ( qp_rdy_o       ),
-   .qp_vld_o       ( qp_vld_o       ),
-   .qp_flag_o      ( qp_flag_o      ),
-   .qp_dt1_o       ( qp_dt1_o       ),
-   .qp_dt2_o       ( qp_dt2_o       ),
-   .p_start_o      ( proc_start_o   ),
-   .p_stop_o       ( proc_stop_o    ),
-   .t_rst_o        ( time_rst_o     ),
-   .t_updt_o       ( time_updt_o    ),
-   .t_updt_dt_o    ( time_updt_dt_o ),
-
-   .c_start_o      ( core_start_o   ),
-   .c_stop_o       ( core_stop_o    ),
-   .xcom_cfg_i     ( xcom_cfg       ),
-   .xcom_id_o      ( xcom_id_o      ),
-   .xcom_mem_o     ( xcom_mem_dt    ),
-   .rx_dt_i        ( xcom_dt_i      ),
-   .rx_ck_i        ( xcom_ck_i      ),
+   .qp_rdy_o       ( o_qp_ready       ),
+   .qp_vld_o       ( o_qp_valid       ),
+   .qp_flag_o      ( o_qp_flag      ),
+   .qp_dt1_o       ( o_qp_data1       ),
+   .qp_dt2_o       ( o_qp_data2       ),
+   .p_start_o      ( o_proc_start   ),
+   .p_stop_o       ( o_proc_stop    ),
+   .t_rst_o        ( o_time_rst     ),
+   .t_updt_o       ( o_time_update    ),
+   .t_updt_dt_o    ( o_time_update_data ),
+   .c_start_o      ( o_core_start   ),
+   .c_stop_o       ( o_core_stop    ),
+   .rx_dt_i        ( i_xcom_data      ),
+   .rx_ck_i        ( i_xcom_clk      ),
    .tx_dt_o        ( tx_dt_s        ),
    .tx_ck_o        ( tx_ck_s        ),
    .xcom_rx_do     ( xcom_rx_ds     ),
@@ -178,56 +218,135 @@ qick_xcom # (
    .xcom_debug_do  ( xcom_debug_ds  )
 );   
 
-
-
+xcom_txrx #(
+.NCH(NCH),
+.SYNC(1'b1)
+)
+u_xcom_txrx
+(
+  .i_clk             ( tb_clk                ),
+  .i_rstn            ( tb_rstn               ),
+  .i_sync            ( i_sync             ),
+  .i_req_loc         ( tb_i_req_loc          ),
+  .i_req_net         ( tb_i_req_net          ),
+  .i_header          ( tb_i_header           ),
+  .i_data            ( tb_i_data             ), 
+  .o_ack_loc         ( tb_o_ack_loc          ),
+  .o_ack_net         ( tb_o_ack_net          ),
+  .o_qp_ready        ( o_qp_ready         ),
+  .o_qp_valid        ( o_qp_valid         ),
+  .o_qp_flag         ( o_qp_flag          ),
+  .o_qp_data1        ( o_qp_data1         ),
+  .o_qp_data2        ( o_qp_data2         ),
+  .o_proc_start      ( o_proc_start       ),
+  .o_proc_stop       ( o_proc_stop        ),
+  .o_time_rst        ( o_time_rst         ),
+  .o_time_update     ( o_time_update      ),
+  .o_time_update_data( o_time_update_data ),
+  .o_core_start      ( o_core_start       ),
+  .o_core_stop       ( o_core_stop        ),
+  .i_cfg_tick        ( xcom_cfg           ),
+  .o_xcom_id         ( o_xcom_id          ),//FIXME: cdc here, check
+  .o_xcom_mem        ( xcom_mem_data        ),//FIXME: review this because here we are crossing clock domains
+  .i_xcom_data       ( i_xcom_data          ),
+  .i_xcom_clk        ( i_xcom_clk           ),
+  .o_xcom_data       ( tb_o_xcom_data        ),
+  .o_xcom_clk        ( tb_o_xcom_clk         ),
+  .o_dbg_rx_data     ( tb_o_dbg_rx_data      ),
+  .o_dbg_tx_data     ( tb_o_dbg_tx_data      ),
+  .o_dbg_status      ( tb_o_dbg_status       ),
+  .o_dbg_data        ( tb_o_dbg_data         )                                                               
+);
 
 
 ///////////////////////////////////////////////////////////////////////////////
 // AXI Registers
 ///////////////////////////////////////////////////////////////////////////////
-axi_slv_xcom XCOM_xREG (
-   .aclk        ( ps_clk             ), 
-   .aresetn     ( ps_aresetn         ), 
-   .awaddr      ( s_axi_awaddr [5:0] ), 
-   .awprot      ( s_axi_awprot       ), 
-   .awvalid     ( s_axi_awvalid      ), 
-   .awready     ( s_axi_awready      ), 
-   .wdata       ( s_axi_wdata        ), 
-   .wstrb       ( s_axi_wstrb        ), 
-   .wvalid      ( s_axi_wvalid       ), 
-   .wready      ( s_axi_wready       ), 
-   .bresp       ( s_axi_bresp        ), 
-   .bvalid      ( s_axi_bvalid       ), 
-   .bready      ( s_axi_bready       ), 
-   .araddr      ( s_axi_araddr       ), 
-   .arprot      ( s_axi_arprot       ), 
-   .arvalid     ( s_axi_arvalid      ), 
-   .arready     ( s_axi_arready      ), 
-   .rdata       ( s_axi_rdata        ), 
-   .rresp       ( s_axi_rresp        ), 
-   .rvalid      ( s_axi_rvalid       ), 
-   .rready      ( s_axi_rready       ), 
-   .XCOM_CTRL   ( XCOM_CTRL          ),
-   .XCOM_CFG    ( XCOM_CFG           ),
-   .AXI_DT1     ( AXI_DT1            ),
-   .AXI_DT2     ( AXI_DT2            ),
-   .AXI_ADDR    ( AXI_ADDR           ),
-   .BOARD_ID    ( xcom_id_o          ),
-   .XCOM_FLAG   ( qp_flag_o          ),
-   .XCOM_DT_1   ( qp_dt1_o           ),
-   .XCOM_DT_2   ( qp_dt2_o           ),
-   .XCOM_MEM    ( axi_mem_dt         ),
-   .XCOM_RX_DT  ( xcom_rx_ds         ),
-   .XCOM_TX_DT  ( xcom_tx_ds         ),
-   .XCOM_STATUS ( xreg_status        ),
-   .XCOM_DEBUG  ( xreg_debug         ));
+xcom_axil_slv #(
+    .C_S_AXI_ADDR_WIDTH = 6,               // Address width.  Adjust as needed.
+    .C_S_AXI_DATA_WIDTH = 32               // Data width (32 or 64).
+) u_axi_slv_xcom(
+   .clk            ( i_ps_clk           ), 
+   .reset_n        ( i_ps_rstn          ), 
+   .s_axi_awaddr   ( s_axi_awaddr [6-1:0] ), 
+   .s_axi_awvalid  ( s_axi_awvalid      ),
+   .s_axi_awready  ( s_axi_awready      ), 
+   .s_axi_wdata    ( s_axi_wdata        ), 
+   .s_axi_wstrb    ( s_axi_wstrb        ), 
+   .s_axi_wvalid   ( s_axi_wvalid       ), 
+   .s_axi_wready   ( s_axi_wready       ), 
+   .s_axi_bresp    ( s_axi_bresp        ), 
+   .s_axi_bvalid   ( s_axi_bvalid       ), 
+   .s_axi_bready   ( s_axi_bready       ), 
+   .s_axi_araddr   ( s_axi_araddr       ), 
+   .s_axi_arvalid  ( s_axi_arvalid      ),
+   .s_axi_arready  ( s_axi_arready      ), 
+   .s_axi_rdata    ( s_axi_rdata        ), 
+   .s_axi_rresp    ( s_axi_rresp        ), 
+   .s_axi_rvalid   ( s_axi_rvalid       ), 
+   .s_axi_rready   ( s_axi_rready       ), 
+   .o_xcom_ctrl    ( s_xcom_ctrl        ), 
+   .o_xcom_cfg     ( s_xcom_cfg         ), 
+   .o_axi_data1    ( s_axi_data1        ),
+   .o_axi_data2    ( s_axi_data2        ),
+   .o_axi_addr     ( s_axi_addr         ),
+   .i_board_id     ( o_xcom_id          ),
+   .i_xcom_flag    ( o_qp_flag          ),
+   .i_xcom_data1   ( o_qp_data1         ),
+   .i_xcom_data2   ( o_qp_data2         ),
+   .i_xcom_mem     ( axi_mem_dt         ),
+   .i_xcom_rx_data ( xcom_rx_ds         ),
+   .i_xcom_tx_data ( xcom_tx_ds         ),
+   .i_xcom_status  ( xreg_status        ),
+   .i_xcom_debug   ( xreg_debug         )
+   ); 
 
+//SYNC STAGES
+///////////////////////////////////////////////////////////////////////////////
+synchronizer#(
+   .NB(6)
+   ) sync_xcom_ctrl(
+  .i_clk      ( i_time_clk        ),
+  .i_rstn     ( i_time_rstn       ),
+  .i_async    ( s_xcom_ctrl[6-1:0]),
+  .o_sync     ( s_xcom_ctrl_sync  )
+);
 
+synchronizer#(
+   .NB(4)
+   ) sync_xcom_cfg(
+  .i_clk      ( i_time_clk        ),
+  .i_rstn     ( i_time_rstn       ),
+  .i_async    ( s_xcom_cfg[4-1:0] ),
+  .o_sync     ( s_xcom_cfg_sync   )
+);
+assign   xcom_cfg = {s_xcom_cfg_sync[3-1:0]+1'b1, 1'b0};
+
+synchronizer#(
+   .NB(32)
+   ) sync_axi_data1(
+  .i_clk      ( i_time_clk        ),
+  .i_rstn     ( i_time_rstn       ),
+  .i_async    ( s_axi_data1       ),
+  .o_sync     ( s_axi_data1_sync  )
+);
+
+synchronizer#(
+   .NB(32)
+   ) sync_axi_data2(
+  .i_clk      ( i_time_clk        ),
+  .i_rstn     ( i_time_rstn       ),
+  .i_async    ( s_axi_data2       ),
+  .o_sync     ( s_axi_data2_sync  )
+);
+assign p_dt   = '{s_axi_data1_sync, s_axi_data2_sync};
+//end of SYNC STAGES
+///////////////////////////////////////////////////////////////////////////////
 // OUTPUTS
 ///////////////////////////////////////////////////////////////////////////////
-assign xcom_dt_o = tx_dt_s;
-assign xcom_ck_o = tx_ck_s;
-assign axi_mem_dt = xcom_mem_dt[AXI_ADDR];
+assign o_xcom_data = tx_dt_s;
+assign o_xcom_clk = tx_ck_s;
+assign axi_mem_dt = xcom_mem_data[s_axi_addr];
 
 ///////////////////////////////////////////////////////////////////////////////
 // DEBUG
