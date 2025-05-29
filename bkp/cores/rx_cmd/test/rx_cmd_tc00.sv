30134eb13a1f345c3b925dd2f941c4eeab2bd09e
Luis Arnaldi <lharnaldi@gmail.com>
2025-05-29 15:12:43 -0500

    Working in the simulation of rx_cmd to take into account the command's meaning

--- bkp/cores/rx_cmd/test/rx_cmd_tc00.sv
+++ bkp/cores/rx_cmd/test/rx_cmd_tc00.sv
@@ -10,7 +10,7 @@ import qick_pkg::*;
 
   localparam CLOCK_FREQUENCY     = 250e6; //[Hz]
 
-  localparam NCH                 = 5;
+  localparam NCH                 = 1;
   localparam NB                  = 32;
 
   logic           tb_clk         = 1'b0;
@@ -36,30 +36,6 @@ import qick_pkg::*;
   logic [NCH-1:0] tb_xcom_data = '0;
   logic [NCH-1:0] tb_xcom_clk  = '0;
 
-  xcom_opcode_t s_op = '{
-       rst         : 5'b1_1111  ,//LOC command 
-       write_mem   : 5'b1_0011  ,//LOC command  
-       write_reg   : 5'b1_0010  ,//LOC command  
-       write_flag  : 5'b1_0001  ,//LOC command  
-       set_id      : 5'b1_0000  ,//LOC command  
-       rfu2        : 5'b0_1111  ,
-       rfu1        : 5'b0_1101  ,
-       qctrl       : 5'b0_1011  ,
-       update_dt32 : 5'b0_1110  ,
-       update_dt16 : 5'b0_1100  ,
-       update_dt8  : 5'b0_1010  ,
-       auto_id     : 5'b0_1001  ,
-       qrst_sync   : 5'b0_1000  ,
-       send_32bit_2: 5'b0_0111  ,
-       send_32bit_1: 5'b0_0110  ,
-       send_16bit_2: 5'b0_0101  ,
-       send_16bit_1: 5'b0_0100  ,
-       send_8bit_2 : 5'b0_0011  ,
-       send_8bit_1 : 5'b0_0010  ,
-       set_flag    : 5'b0_0001  ,
-       clear_flag  : 5'b0_0000  
-  };
-
 initial begin
   $dumpfile("rx_cmd.vcd");
   $dumpvars();
@@ -75,22 +51,21 @@ u_clk_gen
   .o_clk      ( tb_clk            )
 );
 
-
 clocking tb_cb @(posedge tb_clk);
   default input #1step output #2;
-  output  tb_rstn            ;
-  output  tb_i_id            ;
+  output  tb_rstn          ;
+  output  tb_i_id          ;
   output  tb_i_cfg_tick    ;
   output  tb_i_valid       ;
   output  tb_i_header      ;
   output  tb_i_data        ;
 
-  input   tb_o_ready         ;
-  input   tb_o_valid         ;
-  input   tb_o_op            ;
-  input   tb_o_data          ;
-  input   tb_o_chid          ;
-  input   tb_o_dbg_state     ;
+  input   tb_o_ready       ;
+  input   tb_o_valid       ;
+  input   tb_o_op          ;
+  input   tb_o_data        ;
+  input   tb_o_chid        ;
+  input   tb_o_dbg_state   ;
 endclocking
 
 //tx
@@ -98,15 +73,15 @@ genvar ind_tx;
 generate
 for (ind_tx=0; ind_tx < NCH ; ind_tx=ind_tx+1) begin: TX
     xcom_link_tx u_xcom_link_tx(
-        .i_clk      (tb_clk        ),
-        .i_rstn     (tb_rstn       ),
-        .i_cfg_tick (tb_i_cfg_tick ),
-        .i_valid    (tb_i_valid  [ind_tx]  ),
-        .i_header   (tb_i_header [ind_tx]  ),
-        .i_data     (tb_i_data   [ind_tx]  ), 
-        .o_ready    (tb_o_ready  [ind_tx]  ),
-        .o_data     (tb_xcom_data[ind_tx]  ),
-        .o_clk      (tb_xcom_clk [ind_tx]  )
+        .i_clk      (tb_clk               ),
+        .i_rstn     (tb_rstn              ),
+        .i_cfg_tick (tb_i_cfg_tick        ),
+        .i_valid    (tb_i_valid  [ind_tx] ),
+        .i_header   (tb_i_header [ind_tx] ),
+        .i_data     (tb_i_data   [ind_tx] ), 
+        .o_ready    (tb_o_ready  [ind_tx] ),
+        .o_data     (tb_xcom_data[ind_tx] ),
+        .o_clk      (tb_xcom_clk [ind_tx] )
         );
     end
     endgenerate
@@ -187,31 +162,31 @@ task SIM_TX (); begin
    $display("SIM TX");
    tb_cb.tb_i_cfg_tick  <= 2;       //one bit every 2 clock cycles
    @(tb_cb);
-   s_header <= {s_op.auto_id[3:0],4'b0010}; //no data, should trigger timeout. cmd=AUTO_ID, addr board=2
+   s_header <= {XCOM_AUTO_ID,4'b0010}; //no data, should trigger timeout. cmd=AUTO_ID, addr board=2
    s_data   <= 32'd8;
    repeat(10)@(tb_cb);
    TX_ALL();
    
    repeat(20)@(tb_cb);
-   s_header <= {s_op.update_dt8[3:0],4'b0010};//8-bit data. cmd=Update DT8, addr board=2
+   s_header <= {XCOM_UPDATE_DT8,4'b0010};//8-bit data. cmd=Update DT8, addr board=2
    s_data   <= 32'd16;
    repeat(10)@(tb_cb);
    TX_ALL();
  
    repeat(20)@(tb_cb);
-   s_header <= {s_op.update_dt16[3:0],4'b0001};//16-bit data. cmd=Update DT16, addr board=1
+   s_header <= {XCOM_UPDATE_DT16,4'b0001};//16-bit data. cmd=Update DT16, addr board=1
    s_data   <= 32'd24;
    repeat(10)@(tb_cb);
    TX_ALL();
  
    repeat(20)@(tb_cb);
-   s_header <= {s_op.update_dt32[3:0],4'b0010};//32-bit data. cmd=Update DT32, addr board=10
+   s_header <= {XCOM_UPDATE_DT32,4'b0010};//32-bit data. cmd=Update DT32, addr board=10
    s_data   <= 32'd40;
    repeat(10)@(tb_cb);
    TX_ALL();
  
    repeat(20)@(tb_cb);
-   s_header <= {s_op.qrst_sync[3:0],4'b0000};//no data, should trigger timeout. cmd=QRST_SYNC, addr board= broadcast
+   s_header <= {XCOM_QRST_SYNC,4'b0000};//no data, should trigger timeout. cmd=QRST_SYNC, addr board= broadcast
    s_data   <= 32'd40;
    repeat(10)@(tb_cb);
    TX_ALL();
@@ -236,7 +211,7 @@ task TX_DT (input int channel); begin
    wait (tb_cb.tb_o_ready[channel] == 1'b1);
    @ (tb_cb);
    tb_cb.tb_i_valid[channel]     <= 1;
-   wait (tb_cb.tb_o_ready[channel] == 1'b0);
+   //wait (tb_cb.tb_o_ready[channel] == 1'b0);
    @ (tb_cb);
    tb_cb.tb_i_valid[channel]     <= 0;
 end
