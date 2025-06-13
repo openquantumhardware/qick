af1b2d4c873f97df9d4946648b12315f90e2ef9a
Horacio Arnaldi <lharnaldi@gmail.com>
2025-06-13 09:36:33 -0500

    Include the system wrapper in the tcl file for block generation

--- firmware/boards/zcu216/xcom_test_ila_sync/build_bitstream.tcl
+++ firmware/boards/zcu216/xcom_test_ila_sync/build_bitstream.tcl
@@ -8,13 +8,6 @@ set design_name "xcom_test"
 open_project ./${overlay_name}/${overlay_name}.xpr
 open_bd_design ./${overlay_name}/${overlay_name}.srcs/sources_1/bd/${design_name}/${design_name}.bd
 
-# Add top wrapper and xdc files
-make_wrapper -files [get_files ./${overlay_name}/${overlay_name}.srcs/sources_1/bd/${design_name}/${design_name}.bd] -top
-add_files -norecurse ./${overlay_name}/${overlay_name}.srcs/sources_1/bd/${design_name}/hdl/${design_name}_wrapper.v
-set_property top ${design_name}_wrapper [current_fileset]
-import_files -fileset constrs_1 -norecurse ./vivado/constraints/${overlay_name}.xdc
-update_compile_order -fileset sources_1
-
 # set platform properties
 set_property platform.default_output_type "sd_card" [current_project]
 set_property platform.design_intent.embedded "true" [current_project]
