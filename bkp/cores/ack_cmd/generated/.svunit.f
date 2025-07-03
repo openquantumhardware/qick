c4c3b46797de137fe265ba93cfdaf37fbf7fd275
lharnaldi <lharnaldi@gmail.com>
2025-07-03 15:18:09 -0500

    [openquantumhardware/qick_internal#6] Remove fusesoc related directory

--- bkp/cores/ack_cmd/generated/.svunit.f
+++
@@ -1,5 +0,0 @@
-+incdir+/home/arnaldi/git/lharnaldi/pynq_fused/ip/ack_cmd/
-+incdir+/svunit_base/junit-xml
-/svunit_base/junit-xml/junit_xml.sv
-+incdir+/svunit_base
-/svunit_base/svunit_pkg.sv
--- bkp/cores/contrib/sigma_delta_generator/tests/sim/xsim_cfg.tcl
+++
@@ -1,3 +0,0 @@
-log_wave -recursive *
-run all
-exit
