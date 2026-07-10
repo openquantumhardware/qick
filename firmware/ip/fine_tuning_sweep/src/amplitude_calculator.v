c188ea437247faaf7700a2867d2a008cd13d05d7
emrhnozdemir <emirhanozdemir2001@gmail.com>
2026-07-10 16:51:44 +0300

    Update amplitude_calculator.v

--- firmware/ip/fine_tuning_sweep/src/amplitude_calculator.v
+++ firmware/ip/fine_tuning_sweep/src/amplitude_calculator.v
@@ -18,11 +18,8 @@ module amplitude_calculator #(
 );
 
   localparam AMP_WIDTH = 2 * ACC_WIDTH;
-  localparam NLIMB = (ACC_WIDTH + 15) / 16;
-  localparam MAG_WIDTH = 16 * NLIMB;
 
-  integer pj, pk;
-  integer sj, sk;
+  wire rst = ~rst_n;
 
   (* mark_debug = "true" *) reg signed [31:0] i_in, q_in;
   (* mark_debug = "true" *) reg v_in, r_in;
@@ -53,7 +50,7 @@ module amplitude_calculator #(
   wire is_last = (shot_cnt == avg_m1);
   wire emit_now = acc_en & is_last;
 
-  (* mark_debug = "true" *) reg signed [ACC_WIDTH-1:0] sq_in_i, sq_in_q;
+  (* mark_debug = "true" *) reg [ACC_WIDTH-1:0] sq_in_i, sq_in_q;
 
   always @(posedge clk) begin
     if (!rst_n) begin
@@ -80,8 +77,8 @@ module amplitude_calculator #(
           shot_cnt <= 32'd0;
           i_acc <= 0;
           q_acc <= 0;
-          sq_in_i <= i_acc_next;
-          sq_in_q <= q_acc_next;
+          sq_in_i <= i_acc_next[ACC_WIDTH-1] ? (~i_acc_next + 1'b1) : i_acc_next;
+          sq_in_q <= q_acc_next[ACC_WIDTH-1] ? (~q_acc_next + 1'b1) : q_acc_next;
         end else begin
           armed <= 1'b1;
           shot_cnt <= shot_cnt + 32'd1;
@@ -102,99 +99,169 @@ module amplitude_calculator #(
     end
   end
 
-  (* mark_debug = "true" *) reg fin_v0, fin_v1, fin_v2, fin_v3, fin_v4;
+  (* mark_debug = "true" *) reg [ACC_WIDTH-1:0] sq_in_q_d;
 
-  (* mark_debug = "true" *) reg [ACC_WIDTH-1:0] i_mag, q_mag;
-
-  wire [MAG_WIDTH-1:0] i_mag_ext = i_mag;
-  wire [MAG_WIDTH-1:0] q_mag_ext = q_mag;
-
-  reg [31:0] i_diag [0:NLIMB-1];
-  reg [31:0] q_diag [0:NLIMB-1];
-  reg [31:0] i_prod [0:NLIMB-1][0:NLIMB-1];
-  reg [31:0] q_prod [0:NLIMB-1][0:NLIMB-1];
-
-  reg [AMP_WIDTH-1:0] i_squares_r, q_squares_r;
-  reg [AMP_WIDTH-1:0] i_cross_r, q_cross_r;
-
-  (* mark_debug = "true" *) reg [AMP_WIDTH-1:0] i_sq, q_sq;
-
-  reg [AMP_WIDTH-1:0] i_squares_c, q_squares_c;
-  reg [AMP_WIDTH-1:0] i_cross_c, q_cross_c;
-
-  always @(*) begin
-    i_squares_c = {AMP_WIDTH{1'b0}};
-    q_squares_c = {AMP_WIDTH{1'b0}};
-    i_cross_c = {AMP_WIDTH{1'b0}};
-    q_cross_c = {AMP_WIDTH{1'b0}};
-    for (sj = 0; sj < NLIMB; sj = sj + 1) begin
-      i_squares_c = i_squares_c + ({{(AMP_WIDTH-32){1'b0}}, i_diag[sj]} << (32*sj));
-      q_squares_c = q_squares_c + ({{(AMP_WIDTH-32){1'b0}}, q_diag[sj]} << (32*sj));
-      for (sk = sj + 1; sk < NLIMB; sk = sk + 1) begin
-        i_cross_c = i_cross_c + ({{(AMP_WIDTH-32){1'b0}}, i_prod[sj][sk]} << (16*(sj+sk)+1));
-        q_cross_c = q_cross_c + ({{(AMP_WIDTH-32){1'b0}}, q_prod[sj][sk]} << (16*(sj+sk)+1));
-      end
-    end
+  always @(posedge clk) begin
+    if (!rst_n)
+      sq_in_q_d <= 0;
+    else
+      sq_in_q_d <= sq_in_q;
   end
+
+  wire [15:0] il [0:3];
+  wire [15:0] qld [0:3];
+
+  assign il[0] = sq_in_i[15:0];
+  assign il[1] = sq_in_i[31:16];
+  assign il[2] = sq_in_i[47:32];
+  assign il[3] = sq_in_i[63:48];
+
+  assign qld[0] = sq_in_q_d[15:0];
+  assign qld[1] = sq_in_q_d[31:16];
+  assign qld[2] = sq_in_q_d[47:32];
+  assign qld[3] = sq_in_q_d[63:48];
+
+  wire [15:0] ia [0:9];
+  wire [15:0] ib [0:9];
+  wire [15:0] qa [0:9];
+  wire [15:0] qb [0:9];
+
+  assign ia[0] = il[0]; assign ib[0] = il[0];
+  assign ia[1] = il[1]; assign ib[1] = il[1];
+  assign ia[2] = il[2]; assign ib[2] = il[2];
+  assign ia[3] = il[3]; assign ib[3] = il[3];
+  assign ia[4] = il[0]; assign ib[4] = il[1];
+  assign ia[5] = il[0]; assign ib[5] = il[2];
+  assign ia[6] = il[0]; assign ib[6] = il[3];
+  assign ia[7] = il[1]; assign ib[7] = il[2];
+  assign ia[8] = il[1]; assign ib[8] = il[3];
+  assign ia[9] = il[2]; assign ib[9] = il[3];
+
+  assign qa[0] = qld[0]; assign qb[0] = qld[0];
+  assign qa[1] = qld[1]; assign qb[1] = qld[1];
+  assign qa[2] = qld[2]; assign qb[2] = qld[2];
+  assign qa[3] = qld[3]; assign qb[3] = qld[3];
+  assign qa[4] = qld[0]; assign qb[4] = qld[1];
+  assign qa[5] = qld[0]; assign qb[5] = qld[2];
+  assign qa[6] = qld[0]; assign qb[6] = qld[3];
+  assign qa[7] = qld[1]; assign qb[7] = qld[2];
+  assign qa[8] = qld[1]; assign qb[8] = qld[3];
+  assign qa[9] = qld[2]; assign qb[9] = qld[3];
+
+  wire [47:0] comb [0:9];
+
+  genvar t;
+  generate
+    for (t = 0; t < 10; t = t + 1) begin : gpair
+      wire [47:0] pc;
+
+      DSP48E2 #(
+        .A_INPUT("DIRECT"), .B_INPUT("DIRECT"),
+        .USE_MULT("MULTIPLY"), .USE_SIMD("ONE48"), .USE_WIDEXOR("FALSE"),
+        .XORSIMD("XOR24_48_96"),
+        .AUTORESET_PATDET("NO_RESET"), .AUTORESET_PRIORITY("RESET"),
+        .MASK(48'h3fffffffffff), .PATTERN(48'h000000000000),
+        .SEL_MASK("MASK"), .SEL_PATTERN("PATTERN"), .USE_PATTERN_DETECT("NO_PATDET"),
+        .RND(48'h000000000000),
+        .AMULTSEL("A"), .BMULTSEL("B"), .PREADDINSEL("A"),
+        .ACASCREG(0), .ADREG(0), .ALUMODEREG(1), .AREG(0),
+        .BCASCREG(0), .BREG(0), .CARRYINREG(1), .CARRYINSELREG(1),
+        .CREG(1), .DREG(0), .INMODEREG(1), .MREG(0), .OPMODEREG(1), .PREG(1),
+        .IS_ALUMODE_INVERTED(4'b0000), .IS_CARRYIN_INVERTED(1'b0),
+        .IS_CLK_INVERTED(1'b0), .IS_INMODE_INVERTED(5'b00000),
+        .IS_OPMODE_INVERTED(9'b000000000), .IS_RSTALLCARRYIN_INVERTED(1'b0),
+        .IS_RSTALUMODE_INVERTED(1'b0), .IS_RSTA_INVERTED(1'b0),
+        .IS_RSTB_INVERTED(1'b0), .IS_RSTCTRL_INVERTED(1'b0),
+        .IS_RSTC_INVERTED(1'b0), .IS_RSTD_INVERTED(1'b0),
+        .IS_RSTINMODE_INVERTED(1'b0), .IS_RSTM_INVERTED(1'b0),
+        .IS_RSTP_INVERTED(1'b0)
+      ) dsp_a (
+        .ACOUT(), .BCOUT(), .CARRYCASCOUT(), .MULTSIGNOUT(), .PCOUT(pc),
+        .OVERFLOW(), .PATTERNBDETECT(), .PATTERNDETECT(), .UNDERFLOW(),
+        .CARRYOUT(), .P(),
+        .A({14'b0, ia[t]}), .ACIN(30'b0), .ALUMODE(4'b0000), .B({2'b0, ib[t]}),
+        .BCIN(18'b0), .C(48'b0), .CARRYCASCIN(1'b0), .CARRYIN(1'b0),
+        .CARRYINSEL(3'b000), .CLK(clk), .D(27'b0),
+        .INMODE(5'b00000), .MULTSIGNIN(1'b0), .OPMODE(9'b000000101),
+        .PCIN(48'b0),
+        .CEA1(1'b1), .CEA2(1'b1), .CEAD(1'b1), .CEALUMODE(1'b1), .CEB1(1'b1),
+        .CEB2(1'b1), .CEC(1'b1), .CECARRYIN(1'b1), .CECTRL(1'b1), .CED(1'b1),
+        .CEINMODE(1'b1), .CEM(1'b1), .CEP(1'b1),
+        .RSTA(rst), .RSTALLCARRYIN(rst), .RSTALUMODE(rst), .RSTB(rst), .RSTC(rst),
+        .RSTCTRL(rst), .RSTD(rst), .RSTINMODE(rst), .RSTM(rst), .RSTP(rst)
+      );
+
+      DSP48E2 #(
+        .A_INPUT("DIRECT"), .B_INPUT("DIRECT"),
+        .USE_MULT("MULTIPLY"), .USE_SIMD("ONE48"), .USE_WIDEXOR("FALSE"),
+        .XORSIMD("XOR24_48_96"),
+        .AUTORESET_PATDET("NO_RESET"), .AUTORESET_PRIORITY("RESET"),
+        .MASK(48'h3fffffffffff), .PATTERN(48'h000000000000),
+        .SEL_MASK("MASK"), .SEL_PATTERN("PATTERN"), .USE_PATTERN_DETECT("NO_PATDET"),
+        .RND(48'h000000000000),
+        .AMULTSEL("A"), .BMULTSEL("B"), .PREADDINSEL("A"),
+        .ACASCREG(0), .ADREG(0), .ALUMODEREG(1), .AREG(0),
+        .BCASCREG(0), .BREG(0), .CARRYINREG(1), .CARRYINSELREG(1),
+        .CREG(1), .DREG(0), .INMODEREG(1), .MREG(0), .OPMODEREG(1), .PREG(1),
+        .IS_ALUMODE_INVERTED(4'b0000), .IS_CARRYIN_INVERTED(1'b0),
+        .IS_CLK_INVERTED(1'b0), .IS_INMODE_INVERTED(5'b00000),
+        .IS_OPMODE_INVERTED(9'b000000000), .IS_RSTALLCARRYIN_INVERTED(1'b0),
+        .IS_RSTALUMODE_INVERTED(1'b0), .IS_RSTA_INVERTED(1'b0),
+        .IS_RSTB_INVERTED(1'b0), .IS_RSTCTRL_INVERTED(1'b0),
+        .IS_RSTC_INVERTED(1'b0), .IS_RSTD_INVERTED(1'b0),
+        .IS_RSTINMODE_INVERTED(1'b0), .IS_RSTM_INVERTED(1'b0),
+        .IS_RSTP_INVERTED(1'b0)
+      ) dsp_b (
+        .ACOUT(), .BCOUT(), .CARRYCASCOUT(), .MULTSIGNOUT(), .PCOUT(),
+        .OVERFLOW(), .PATTERNBDETECT(), .PATTERNDETECT(), .UNDERFLOW(),
+        .CARRYOUT(), .P(comb[t]),
+        .A({14'b0, qa[t]}), .ACIN(30'b0), .ALUMODE(4'b0000), .B({2'b0, qb[t]}),
+        .BCIN(18'b0), .C(48'b0), .CARRYCASCIN(1'b0), .CARRYIN(1'b0),
+        .CARRYINSEL(3'b000), .CLK(clk), .D(27'b0),
+        .INMODE(5'b00000), .MULTSIGNIN(1'b0), .OPMODE(9'b000010101),
+        .PCIN(pc),
+        .CEA1(1'b1), .CEA2(1'b1), .CEAD(1'b1), .CEALUMODE(1'b1), .CEB1(1'b1),
+        .CEB2(1'b1), .CEC(1'b1), .CECARRYIN(1'b1), .CECTRL(1'b1), .CED(1'b1),
+        .CEINMODE(1'b1), .CEM(1'b1), .CEP(1'b1),
+        .RSTA(rst), .RSTALLCARRYIN(rst), .RSTALUMODE(rst), .RSTB(rst), .RSTC(rst),
+        .RSTCTRL(rst), .RSTD(rst), .RSTINMODE(rst), .RSTM(rst), .RSTP(rst)
+      );
+    end
+  endgenerate
+
+  wire [AMP_WIDTH-1:0] term0 = {{(AMP_WIDTH-48){1'b0}}, comb[0]};
+  wire [AMP_WIDTH-1:0] term1 = {{(AMP_WIDTH-48){1'b0}}, comb[1]} << 32;
+  wire [AMP_WIDTH-1:0] term2 = {{(AMP_WIDTH-48){1'b0}}, comb[2]} << 64;
+  wire [AMP_WIDTH-1:0] term3 = {{(AMP_WIDTH-48){1'b0}}, comb[3]} << 96;
+  wire [AMP_WIDTH-1:0] term4 = {{(AMP_WIDTH-48){1'b0}}, comb[4]} << 17;
+  wire [AMP_WIDTH-1:0] term5 = {{(AMP_WIDTH-48){1'b0}}, comb[5]} << 33;
+  wire [AMP_WIDTH-1:0] term6 = {{(AMP_WIDTH-48){1'b0}}, comb[6]} << 49;
+  wire [AMP_WIDTH-1:0] term7 = {{(AMP_WIDTH-48){1'b0}}, comb[7]} << 49;
+  wire [AMP_WIDTH-1:0] term8 = {{(AMP_WIDTH-48){1'b0}}, comb[8]} << 65;
+  wire [AMP_WIDTH-1:0] term9 = {{(AMP_WIDTH-48){1'b0}}, comb[9]} << 81;
+
+  wire [AMP_WIDTH-1:0] grp0 = term0 + term1 + term2;
+  wire [AMP_WIDTH-1:0] grp1 = term3 + term4 + term5;
+  wire [AMP_WIDTH-1:0] grp2 = term6 + term7 + term8;
+  wire [AMP_WIDTH-1:0] grp3 = term9;
+  wire [AMP_WIDTH-1:0] lvl0 = grp0 + grp1 + grp2;
+  wire [AMP_WIDTH-1:0] sum128 = lvl0 + grp3;
+
+  (* mark_debug = "true" *) reg fin_v0, fin_v1, fin_v2;
 
   always @(posedge clk) begin
     if (!rst_n) begin
       fin_v0 <= 1'b0;
       fin_v1 <= 1'b0;
       fin_v2 <= 1'b0;
-      fin_v3 <= 1'b0;
-      fin_v4 <= 1'b0;
-      i_mag <= 0;
-      q_mag <= 0;
-      for (pj = 0; pj < NLIMB; pj = pj + 1) begin
-        i_diag[pj] <= 32'd0;
-        q_diag[pj] <= 32'd0;
-        for (pk = 0; pk < NLIMB; pk = pk + 1) begin
-          i_prod[pj][pk] <= 32'd0;
-          q_prod[pj][pk] <= 32'd0;
-        end
-      end
-      i_squares_r <= 0;
-      q_squares_r <= 0;
-      i_cross_r <= 0;
-      q_cross_r <= 0;
-      i_sq <= 0;
-      q_sq <= 0;
       m_axis_tdata <= 0;
       m_axis_tvalid <= 1'b0;
     end else begin
       fin_v0 <= emit_now;
-      i_mag <= sq_in_i[ACC_WIDTH-1] ? (~sq_in_i + 1'b1) : sq_in_i;
-      q_mag <= sq_in_q[ACC_WIDTH-1] ? (~sq_in_q + 1'b1) : sq_in_q;
-
       fin_v1 <= fin_v0;
-      for (pj = 0; pj < NLIMB; pj = pj + 1) begin
-        i_diag[pj] <= i_mag_ext[16*pj +: 16] * i_mag_ext[16*pj +: 16];
-        q_diag[pj] <= q_mag_ext[16*pj +: 16] * q_mag_ext[16*pj +: 16];
-        for (pk = 0; pk < NLIMB; pk = pk + 1) begin
-          if (pk > pj) begin
-            i_prod[pj][pk] <= i_mag_ext[16*pj +: 16] * i_mag_ext[16*pk +: 16];
-            q_prod[pj][pk] <= q_mag_ext[16*pj +: 16] * q_mag_ext[16*pk +: 16];
-          end else begin
-            i_prod[pj][pk] <= i_prod[pj][pk];
-            q_prod[pj][pk] <= q_prod[pj][pk];
-          end
-        end
-      end
-
       fin_v2 <= fin_v1;
-      i_squares_r <= i_squares_c;
-      q_squares_r <= q_squares_c;
-      i_cross_r <= i_cross_c;
-      q_cross_r <= q_cross_c;
-
-      fin_v3 <= fin_v2;
-      i_sq <= i_squares_r + i_cross_r;
-      q_sq <= q_squares_r + q_cross_r;
-
-      fin_v4 <= fin_v3;
-      m_axis_tvalid <= fin_v4;
-      m_axis_tdata <= i_sq + q_sq;
+      m_axis_tvalid <= fin_v2;
+      m_axis_tdata <= sum128;
     end
   end
 
