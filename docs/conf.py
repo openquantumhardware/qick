ca9939d6b0a4b30f0321db27fd285512d8acce05
Sho Uemura <suemura@fnal.gov>
2026-09-08 17:00:17 -0500

    add mock import for pmbus, to unbreak readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,14 @@ autodoc_default_options = {
     'show-inheritance': True,
 }
 
-autodoc_mock_imports = ["pynq", "xrfclk", "xrfdc", "cffi", "Pyro4", "psutil"]
+autodoc_mock_imports = ["pynq",
+                        "xrfclk",
+                        "xrfdc",
+                        "cffi",
+                        "Pyro4",
+                        "psutil",
+                        "qick.board_utils.pmbus",
+                        ]
 
 ## Generate autodoc stubs with summaries from code
 autosummary_generate = True
